3d92badcfe82f97b9eb059c2754acabc4847069e
Alan Churley <achurley@amazon.co.uk>
2020-10-28 17:02:45 +0000

    Adding coverage reporter

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -2,6 +2,6 @@ import { Hello } from '../index';
 describe('Simple expression tests', () => {
   test('Hello', () => {
     expect(Hello('Carl')).toBe('Hello Carl');
-    expect(Hello()).toBe('Not Hello unknown person');
+    expect(Hello()).toBe('Hello unknown person');
   });
 });

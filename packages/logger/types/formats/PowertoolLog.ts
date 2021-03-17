f138058fb3aa637de9b6e0719a072dd500085fa7
Sara Gerion <sgerion@amazon.com>
2021-03-17 09:35:54 +0100

    chore(logger): Lambda first letter uppercase

--- packages/logger/types/formats/PowertoolLog.ts
+++ packages/logger/types/formats/PowertoolLog.ts
@@ -45,7 +45,7 @@ type PowertoolLog = LogAttributes & {
   /**
    * xray_trace_id
    *
-   * Description: X-Ray Trace ID when lambda function has enabled Tracing.
+   * Description: X-Ray Trace ID when Lambda function has enabled Tracing.
    * Example: "1-5759e988-bd862e3fe1be46a994272793"
    */
   xray_trace_id?: string
@@ -61,7 +61,7 @@ type PowertoolLog = LogAttributes & {
   /**
    * lambda_function_name
    *
-   * Description: The name of the lambda function.
+   * Description: The name of the Lambda function.
    * Example: "example-powertools-HelloWorldFunction-1P1Z6B39FLU73"
    */
   lambda_function_name?: string
@@ -69,7 +69,7 @@ type PowertoolLog = LogAttributes & {
   /**
    * lambda_function_memory_size
    *
-   * Description: The memory size of the lambda function.
+   * Description: The memory size of the Lambda function.
    * Example: 128
    */
   lambda_function_memory_size?: number
@@ -77,7 +77,7 @@ type PowertoolLog = LogAttributes & {
   /**
    * lambda_function_arn
    *
-   * Description: The ARN of the lambda function.
+   * Description: The ARN of the Lambda function.
    * Example: "arn:aws:lambda:eu-west-1:012345678910:function:example-powertools-HelloWorldFunction-1P1Z6B39FLU73"
    */
   lambda_function_arn?: string

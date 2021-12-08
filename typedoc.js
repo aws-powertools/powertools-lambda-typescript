628d9ce8d1dd3df29f6bb508c0ba197a3ca44317
ijemmy <ijemmy@users.noreply.github.com>
2021-12-08 16:26:13 +0100

    feat: generate API documentation with typedoc

---
+++ typedoc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  out: 'api',
+  exclude: ['**/node_modules/**', '**/*.test.ts', '**/*.json'],
+  name: 'aws-lambda-powertools-typescript',
+  excludePrivate: true,
+  entryPointStrategy: 'packages',
+};

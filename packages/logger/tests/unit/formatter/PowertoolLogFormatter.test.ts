8a5bcc06c0fda368410269f3fa5569f318de50ac
Andrea Amorosi <dreamorosi@gmail.com>
2021-12-14 11:52:22 +0100

    Applied fix to PowertoolLogFormatter see #304

--- packages/logger/tests/unit/formatter/PowertoolLogFormatter.test.ts
+++ packages/logger/tests/unit/formatter/PowertoolLogFormatter.test.ts
@@ -246,11 +246,12 @@ describe('Class: PowertoolLogFormatter', () => {
       try {
         shouldThrow();
       } catch (error) {
+        // TODO: review message content assertion (see Issue #304)
         // Assess
         const formattedTypeError = formatter.formatError(error);
         expect(formattedTypeError).toEqual({
           location: expect.stringMatching(/PowertoolLogFormatter.test.ts:[0-9]+/),
-          message: 'Cannot read property \'foo\' of null',
+          message: expect.stringMatching(/Cannot read propert/),
           name: 'TypeError',
           stack: expect.stringMatching(/PowertoolLogFormatter.test.ts:[0-9]+:[0-9]+/),
         });

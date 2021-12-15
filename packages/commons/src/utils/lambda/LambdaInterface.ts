b91ae36c2478fb7e5bfb766eac50b26351367225
Florian CHAZAL <florianchazal@gmail.com>
2021-12-15 11:27:56 +0100

    chore(commons): Create a common package

---
+++ packages/commons/src/utils/lambda/LambdaInterface.ts
@@ -0,0 +1,9 @@
+import { Handler } from 'aws-lambda';
+
+interface LambdaInterface {
+  handler: Handler;
+}
+
+export {
+  LambdaInterface,
+};
---
+++ packages/commons/tests/unit/LambdaInterface.test.ts
@@ -0,0 +1,20 @@
+import { Callback, Context } from 'aws-lambda';
+import { ContextExamples, LambdaInterface } from '../../src';
+
+describe('LambdaInterface', () => {
+  test('it compiles', async () => {
+    class LambdaFunction implements LambdaInterface {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      public handler<TEvent, TResult>(
+        _event: TEvent,
+        _context: Context,
+        _callback: Callback<TResult>,
+      ): void | Promise<TResult> {
+        return;
+      }
+    }
+
+    await new LambdaFunction().handler({}, ContextExamples.helloworldContext, () => console.log('Lambda invoked!'));
+  });
+});

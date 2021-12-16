db6ad64b9eaf5d92832299be5abaef145e17ae4f
Sara Gerion <sgerion@amazon.com>
2021-12-16 22:19:14 +0100

    chore: linting fix

--- packages/logger/src/types/Log.ts
+++ packages/logger/src/types/Log.ts
@@ -13,7 +13,7 @@ type LogAttributeValue = string | number | boolean | null | undefined | LogAttri
 type LogAttributes = { [key: string]: LogAttributeValue };
 
 type LogAttributesWithMessage = LogAttributes & {
-  message: string;
+  message: string
 };
 
 type Environment = 'dev' | 'local' | 'staging' | 'prod' | string;

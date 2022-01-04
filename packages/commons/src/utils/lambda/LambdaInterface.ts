45edbac29b29da062b2446cb7c579f10a997b85c
Florian Chazal <florianchazal@gmail.com>
2022-01-04 10:47:44 +0100

    create ASync and Sync Handlers (#394)

--- packages/commons/src/utils/lambda/LambdaInterface.ts
+++ packages/commons/src/utils/lambda/LambdaInterface.ts
@@ -1,9 +1,20 @@
 import { Handler } from 'aws-lambda';
 
+export type SyncHandler<T extends Handler> = (
+  event: Parameters<T>[0],
+  context: Parameters<T>[1],
+  callback: Parameters<T>[2],
+) => void;
+
+export type AsyncHandler<T extends Handler> = (
+  event: Parameters<T>[0],
+  context: Parameters<T>[1],
+) => Promise<NonNullable<Parameters<Parameters<T>[2]>[1]>>;
+
 interface LambdaInterface {
-  handler: Handler
+  handler: SyncHandler<Handler> | AsyncHandler<Handler>
 }
 
 export {
-  LambdaInterface,
+  LambdaInterface
 };

a8594b9ec323cee3a8925194451a81ff5eb0fe20
Sara Gerion <47529391+saragerion@users.noreply.github.com>
2022-01-04 01:28:20 +0100

    docs(logger): improve mkdocs and examples of sample rate feature (#389)

--- packages/logger/examples/sample-rate.ts
+++ packages/logger/examples/sample-rate.ts
@@ -2,7 +2,7 @@
 require('./../tests/helpers/populateEnvironmentVariables');
 
 // Additional runtime variables
-process.env.LOG_LEVEL = 'WARN';
+process.env.LOG_LEVEL = 'ERROR';
 process.env.POWERTOOLS_SERVICE_NAME = 'hello-world';
 process.env.POWERTOOLS_LOGGER_SAMPLE_RATE = '0.5';
 
@@ -15,13 +15,18 @@ const logger = new Logger();
 
 const lambdaHandler: Handler = async () => {
 
-  logger.info('This is INFO log #1');
-  logger.info('This is INFO log #2');
-  logger.info('This is INFO log #3');
-  logger.info('This is INFO log #4');
+  // This log item (equal to log level 'ERROR') will be printed to standard output
+  // in all Lambda invocations
+  logger.error('This is an ERROR log');
 
-  // Refresh sample rate calculation on runtime
-  logger.refreshSampleRateCalculation();
+  // These log items (below the log level 'ERROR') have ~50% chance
+  // of being printed in a Lambda invocation
+  logger.debug('This is a DEBUG log that has 50% chance of being printed');
+  logger.info('This is an INFO log that has 50% chance of being printed');
+  logger.warn('This is a WARN log that has 50% chance of being printed');
+
+  // Optional: refresh sample rate calculation on runtime
+  // logger.refreshSampleRateCalculation();
 
   return {
     foo: 'bar'

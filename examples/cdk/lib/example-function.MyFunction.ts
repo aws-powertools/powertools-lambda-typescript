bc77747fb98917fab5786aadce5e19ea1f46ea4d
Grzegorz Kozub <kozubg@gmail.com>
2022-01-06 18:08:49 +0100

    refactor(metrics): update usage examples  to follow new naming convention of throwOnEmptyMetrics property

--- examples/cdk/lib/example-function.MyFunction.ts
+++ examples/cdk/lib/example-function.MyFunction.ts
@@ -29,7 +29,7 @@ export const handler = async (_event: unknown, context: Context): Promise<void> 
 
   // ### Experiment metrics
   metrics.captureColdStartMetric();
-  metrics.raiseOnEmptyMetrics();
+  metrics.throwOnEmptyMetrics();
   metrics.setDefaultDimensions({ environment: 'example', type: 'standardFunction' });
   metrics.addMetric('test-metric', MetricUnits.Count, 10);
 
@@ -38,7 +38,7 @@ export const handler = async (_event: unknown, context: Context): Promise<void> 
   metricWithItsOwnDimensions.addMetric('single-metric', MetricUnits.Percent, 50);
 
   metrics.purgeStoredMetrics();
-  metrics.raiseOnEmptyMetrics();
+  metrics.throwOnEmptyMetrics();
 
   // ### Experiment tracer
 

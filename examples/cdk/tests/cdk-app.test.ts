2852495933b01ababd5092c057e2a548711b7821
Florian CHAZAL <florianchazal@gmail.com>
2022-01-17 13:39:39 +0100

    bump to 0.3.1 in example

--- examples/cdk/tests/cdk-app.test.ts
+++ examples/cdk/tests/cdk-app.test.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as CdkApp from '../src/example-stack';
 
-test('It Deploys', () => {
+test('CDK code synthesize', () => {
   const app = new cdk.App();
   const stack = new CdkApp.CdkAppStack(app, 'MyTestStack');
   expect(Template.fromStack(stack)).toMatchSnapshot();

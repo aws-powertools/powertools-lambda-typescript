57c5929b198e51e796d6a3d37d982493375b8f66
Sara Gerion <sgerion@amazon.com>
2022-01-19 21:54:30 +0100

    feat(logger): add jsdocs support

--- packages/logger/src/middleware/middy.ts
+++ packages/logger/src/middleware/middy.ts
@@ -1,6 +1,13 @@
 import type { Logger } from '../Logger';
 import middy from '@middy/core';
 
+/**
+ * Middy middleware that adds the current Lambda invocation's context
+ * inside all log items.
+ *
+ * @param {Logger|Logger[]} target
+ * @returns middy.MiddlewareObj
+ */
 const injectLambdaContext = (target: Logger | Logger[]): middy.MiddlewareObj => {
   const injectLambdaContextBefore = async (request: middy.Request): Promise<void> => {
     const loggers = target instanceof Array ? target : [target];

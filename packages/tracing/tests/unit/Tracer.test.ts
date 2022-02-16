c51dfd0dadfb6f3c7070adedf7a2198e9b4c37af
ijemmy <ijemmy@users.noreply.github.com>
2022-02-16 17:19:19 +0100

    chore(all): replace eu-central-1 with eu-west-1 in tracer and metrics packages

--- packages/tracing/tests/unit/Tracer.test.ts
+++ packages/tracing/tests/unit/Tracer.test.ts
@@ -31,7 +31,7 @@ describe('Class: Tracer', () => {
     memoryLimitInMB: '128',
     logGroupName: '/aws/lambda/foo-bar-function-123456abcdef',
     logStreamName: '2021/03/09/[$LATEST]abcdef123456abcdef123456abcdef123456',
-    invokedFunctionArn: 'arn:aws:lambda:eu-central-1:123456789012:function:Example',
+    invokedFunctionArn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
     awsRequestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
     getRemainingTimeInMillis: () => 1234,
     done: () => console.log('Done!'),

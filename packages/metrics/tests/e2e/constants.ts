8ae5f52ff19e6afe4a1d1d13bce05fcff173d40d
ijemmy <ijemmy@users.noreply.github.com>
2022-05-02 15:32:57 +0200

    test(all): extend timeout or it sometimes fail on GitHub action

--- packages/metrics/tests/e2e/constants.ts
+++ packages/metrics/tests/e2e/constants.ts
@@ -1,5 +1,5 @@
 export const RESOURCE_NAME_PREFIX = 'Metrics-E2E';
-export const ONE_MINUTE = 60 * 10_00;
-export const TEST_CASE_TIMEOUT = 90_000; // 90 seconds
-export const SETUP_TIMEOUT = 300_000; // 300 seconds
-export const TEARDOWN_TIMEOUT = 200_000;
+export const ONE_MINUTE = 60 * 1000;
+export const TEST_CASE_TIMEOUT = 3 * ONE_MINUTE;
+export const SETUP_TIMEOUT = 5 * ONE_MINUTE;
+export const TEARDOWN_TIMEOUT = 5 * ONE_MINUTE;

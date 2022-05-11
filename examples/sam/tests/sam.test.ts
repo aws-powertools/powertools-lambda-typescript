e3b11c0903e1ec621048b9dceeec01c4e8bdd2ea
Andrea Amorosi <dreamorosi@gmail.com>
2022-05-11 17:46:48 +0200

    Added jest to sam

--- examples/sam/tests/sam.test.ts
+++ examples/sam/tests/sam.test.ts
@@ -1,22 +1,22 @@
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { getAllItemsHandler } from '../src/get-all-items';
 import { getByIdHandler } from '../src/get-by-id';
 import { putItemHandler } from '../src/put-item';
 
-test('CDK code synthesize', () => {
-  expect(getAllItemsHandler({} as APIGatewayProxyEvent, {} as Context)).toThrow(
+test('getAllItemsHandler function imports & throws correctly', () => {
+  expect(getAllItemsHandler({} as APIGatewayProxyEvent, {} as Context)).rejects.toThrow(
     'getAllItems only accepts GET method, you tried: undefined'
   );
 });
 
-test('CDK code synthesize', () => {
-  expect(getByIdHandler({} as APIGatewayProxyEvent, {} as Context)).toThrow(
+test('getByIdHandler function imports & throws correctly', () => {
+  expect(getByIdHandler({} as APIGatewayProxyEvent, {} as Context)).rejects.toThrow(
     'getByIdHandler only accepts GET method, you tried: undefined'
   );
 });
 
-test('CDK code synthesize', () => {
-  expect(putItemHandler({} as APIGatewayProxyEvent, {} as Context)).toThrow(
+test('putItemHandler function imports & throws correctly', () => {
+  expect(putItemHandler({} as APIGatewayProxyEvent, {} as Context)).rejects.toThrow(
     'putItemHandler only accepts POST method, you tried: undefined'
   );
 });

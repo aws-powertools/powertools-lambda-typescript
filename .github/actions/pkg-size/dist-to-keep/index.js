022f70ecd8c754ac34f4d8dfb86285afedb0337a
Florian Chazal <chazalf@amazon.com>
2022-05-13 11:33:32 +0200

    next

--- .github/actions/pkg-size/dist-to-keep/index.js
+++ .github/actions/pkg-size/dist-to-keep/index.js
@@ -14091,9 +14091,7 @@ async function buildRef({
   if (checkoutRef) {
     import_core.info(`Checking out ref '${checkoutRef}'`);
     await exec_default(`git checkout -f ${checkoutRef}`);
-    await exec_default(`npm --version`, { cwd });
-    await exec_default(`node --version`, { cwd });
-    await exec_default(`npm ci  --foreground-scripts`, { cwd });
+    await exec_default(`npm ci  --foreground-scripts --verbose`, { cwd });
   }
   if (buildCommand !== "false") {
     if (!buildCommand) {

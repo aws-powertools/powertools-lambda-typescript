4715678b78287115776df84f4b8558ee867447f7
Florian Chazal <florianchazal@gmail.com>
2022-05-25 14:25:34 +0200

    fix(commons): rename tests subfolder to samples to avoid being deleted by tools such as node-prune (#882)
    * rename commons sub folder from tests to samples
    
    * update doc
    
    Co-authored-by: Florian Chazal <chazalf@amazon.com>

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -1,4 +1,4 @@
 export * from './utils/lambda';
 export * from './Utility';
-export * as ContextExamples from './tests/resources/contexts';
-export * as Events from './tests/resources/events';
+export * as ContextExamples from './samples/resources/contexts';
+export * as Events from './samples/resources/events';

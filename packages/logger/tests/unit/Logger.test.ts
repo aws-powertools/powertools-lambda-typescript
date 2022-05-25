6e1eda0667efec8c52978baa69485b3374a5af9c
Sara Gerion <47529391+saragerion@users.noreply.github.com>
2022-05-25 23:37:22 +0200

    Update packages/logger/tests/unit/Logger.test.ts
    Co-authored-by: Andrea Amorosi <dreamorosi@gmail.com>

--- packages/logger/tests/unit/Logger.test.ts
+++ packages/logger/tests/unit/Logger.test.ts
@@ -638,7 +638,7 @@ describe('Class: Logger', () => {
 
   describe('Method: appendKeys', () => {
 
-    test('when called, it populates the logger\'s propriety persistentLogAttributes ', () => {
+    test('when called, it populates the logger\'s persistentLogAttributes property', () => {
 
       // Prepare
       const logger = new Logger();

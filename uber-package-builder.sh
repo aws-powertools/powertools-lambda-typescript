8c42b7b9a73e351b08a11ba598349830c86aa2df
Florian Chazal <chazalf@amazon.com>
2022-05-30 17:05:40 +0200

    Add lerna command for uber zip

--- uber-package-builder.sh
+++ uber-package-builder.sh
@@ -2,73 +2,45 @@
 set -e
 
 
-usage() {
-  echo "Uber package Builder"
-  echo "------------------------"
-  echo "./uber-pacakge-builder.sh NAME LOCAL_NPM_PACKAGE_LOCATION"
-  echo ""}
+# usage() {
+#   echo "Uber package Builder"
+#   echo "------------------------"
+#   echo "./uber-pacakge-builder.sh NAME LOCAL_NPM_PACKAGE_LOCATION"
+#   echo ""}
 
 
-if [[ "$#" -lt 3 ]]; then
-   usage
-   exit 1
-fi
+# if [[ "$#" -lt 2 ]]; then
+#    usage
+#    exit 1
+# fi
 
-name="${1}"
-runtime="${2}"
-dist_folder="${3}"
+name=$(basename ${1})
+dist_folder="${2}"
 
-if test -d "$dist_folder"; then
-  packages="${@:4}"
-else
-  dist_folder=""
-  packages="${@:3}"
-fi
-
+echo "Will bundle $(ls ${dist_folder}) into ${dist_folder}/${name}.tgz from ${PWD}"
 
 output_folder="$(mktemp -d)"
 
 echo "$output_folder is the output folder"
 
-docker_image="public.ecr.aws/sam/build-$runtime:latest"
-volume_params="-v $output_folder:/layer"
+docker_image="public.ecr.aws/sam/build-nodejs14.x:latest"
+volume_params="-v $output_folder:/bundle"
 
-if [[ $runtime == node* ]]; then
+package_folder="nodejs/"
+mkdir -p "$output_folder/$package_folder"
 
-  package_folder="nodejs/"
-  mkdir -p "$output_folder/$package_folder"
-  if [[ -n "$dist_folder" ]]; then
-    cp -r "$dist_folder" "$output_folder/$package_folder/"
-  fi
-  install_command="pushd $package_folder; npm install; npm install --save $packages; popd"
-  volume_params="$volume_params -v $HOME/.npmrc:/root/.npmrc"
+cp -r "${2}" "$output_folder/$package_folder/"
+ 
+install_command="pushd $package_folder; npm install --save ./*.tgz; popd"
+volume_params="$volume_params -v $HOME/.npmrc:/root/.npmrc"
 
-elif [[ $runtime == python* ]]; then
+# echo "Building bundle: docker run --rm $volume_params -w "/bundle" "$docker_image" /bin/bash -c "$install_command && $zip_command""
+zip_command="zip -r bundle.zip * && rm -rf $package_folder"
 
-  package_folder="python/lib/$runtime/site-packages/"
-  if [[ -n "$dist_folder" ]]; then
-    cp "$dist_folder" "$output_folder/requirements.txt"
-  else
-    touch "$output_folder/requirements.txt"
-  fi
+docker run --rm $volume_params -w "/bundle" "$docker_image" /bin/bash -c "$install_command && $zip_command"
 
-  install_command="pip install -r requirements.txt -t $package_folder $packages"
-  volume_params="$volume_params -v $HOME/.config/pip:/root/.config/pip"
+mv "$output_folder/bundle.zip" "$dist_folder/$name.zip"
 
-else
+rm -rf $output_folder
 
-  usage
-  exit 1
-
-fi
-
-
-echo "Building layer"
-zip_command="zip -r layer.zip * && rm -rf $package_folder"
-
-docker run --rm $volume_params -w "/layer" "$docker_image" /bin/bash -c "$install_command && $zip_command"
-
-
-mv "$output_folder/layer.zip" "$dist_folder/$name.zip"
-
-echo "All done. Enjoy your shiny new Lambda layer in $output_folder !"
+echo "All done"

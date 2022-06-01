d78f739b887079bc8172c6889cc22e745ac472a4
Florian Chazal <chazalf@amazon.com>
2022-06-01 17:53:45 +0200

    rename uber to bundle

--- package-bundler.sh
+++ package-bundler.sh
@@ -2,26 +2,25 @@
 set -e
 
 
-# usage() {
-#   echo "Uber package Builder"
-#   echo "------------------------"
-#   echo "./uber-pacakge-builder.sh NAME LOCAL_NPM_PACKAGE_LOCATION"
-#   echo ""}
+usage() {
+  echo "Uber package Builder"
+  echo "------------------------"
+  echo "./package-bundler.sh NAME LOCAL_NPM_PACKAGE_LOCATION"
+  echo ""
+}
 
 
-# if [[ "$#" -lt 2 ]]; then
-#    usage
-#    exit 1
-# fi
+if [[ "$#" -lt 2 ]]; then
+  usage
+  exit 1
+fi
 
 name=$(basename ${1})
 dist_folder="${2}"
 
-echo "Will bundle $(ls ${dist_folder}) into ${dist_folder}/${name}.tgz from ${PWD}"
+echo "Will bundle $(ls ${dist_folder}) into ${dist_folder}/${name}.tgz"
 
 output_folder="$(mktemp -d)"
-
-echo "$output_folder is the output folder"
 
 docker_image="public.ecr.aws/sam/build-nodejs14.x:latest"
 volume_params="-v $output_folder:/bundle"
@@ -34,7 +33,6 @@ cp -r "${2}" "$output_folder/$package_folder/"
 install_command="pushd $package_folder; npm install --save ./*.tgz; popd"
 volume_params="$volume_params -v $HOME/.npmrc:/root/.npmrc"
 
-# echo "Building bundle: docker run --rm $volume_params -w "/bundle" "$docker_image" /bin/bash -c "$install_command && $zip_command""
 zip_command="zip -r bundle.zip * && rm -rf $package_folder"
 
 docker run --rm $volume_params -w "/bundle" "$docker_image" /bin/bash -c "$install_command && $zip_command"

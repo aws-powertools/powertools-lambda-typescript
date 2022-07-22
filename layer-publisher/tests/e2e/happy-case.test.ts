1e58383fd181bbb9a563ad5b07a7c79da42f0065
Florian Chazal <chazalf@amazon.com>
2022-07-22 18:48:09 +0200

    fix comments

--- layer-publisher/tests/e2e/happy-case.test.ts
+++ layer-publisher/tests/e2e/happy-case.test.ts
@@ -9,9 +9,9 @@ import * as cdk from 'aws-cdk-lib';
 import { Stack } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as LayerPublisher from '../../src/layer-publisher-stack';
-import { deployStack, destroyStack } from './utils/cdk-cli';
-import { generateUniqueName, invokeFunction } from './utils/e2eUtils';
-import { LEVEL } from './utils/InvocationLogs';
+import { deployStack, destroyStack } from '../../../packages/commons/tests/utils/cdk-cli';
+import { generateUniqueName, invokeFunction } from '../../../packages/commons/tests/utils/e2eUtils';
+import { LEVEL } from '../../../packages/commons/tests/utils/InvocationLogs';
 
 const runtime = lambda.Runtime.ALL.find((r) => r.name === process.env.RUNTIME) ?? lambda.Runtime.NODEJS_14_X;
 

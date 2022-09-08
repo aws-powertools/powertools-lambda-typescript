1218fabc2e26931950a6b52733cc1b8d42cb819a
vgphoenixcampos <111440293+vgphoenixcampos@users.noreply.github.com>
2022-09-08 17:03:56 -0400

    feat: removing unused params from options

--- packages/idempotency/src/IdempotencyOptions.ts
+++ packages/idempotency/src/IdempotencyOptions.ts
@@ -1,9 +1,7 @@
-import { IdempotencyPersistenceConfig } from './IdempotencyPersistenceConfig';
 import { PersistenceLayer } from './persistence/PersistenceLayer';
 
 type IdempotencyOptions = {
   dataKeywordArgument: string
-  config: IdempotencyPersistenceConfig
   persistenceStore: PersistenceLayer
 };
 

3d1f52ceb4103c62890d86b941c97be439c88759
KevenFuentes9 <keven_fuentes@vanguard.com>
2022-09-13 22:08:50 -0400

    feat: allow for data attr to be passed and return in persistence layer get

--- packages/idempotency/src/persistence/DynamoDbPersistenceLayer.ts
+++ packages/idempotency/src/persistence/DynamoDbPersistenceLayer.ts
@@ -3,19 +3,21 @@ import { PersistenceLayer } from './PersistenceLayer';
 import { IdempotencyRecord } from './IdempotencyRecord';
 
 class DynamoDBPersistenceLayer extends PersistenceLayer {
-  public constructor(private tableName: string, private key_attr: string = 'id', private status_attr: string = 'status', private expiry_attr: string = 'expiration', private in_progress_expiry_attr: string = 'in_progress_expiry_attr') {
+  public constructor(private tableName: string, private key_attr: string = 'id', 
+    private status_attr: string = 'status', private expiry_attr: string = 'expiration', 
+    private in_progress_expiry_attr: string = 'in_progress_expiry_attr',
+    private data_attr: string = 'data' ) {
     super();
   }
   protected async _deleteRecord(): Promise<void> {}
   protected async _getRecord(idempotencyKey: string): Promise<IdempotencyRecord> {
-    const client: DynamoDB = new DynamoDB({});
-    const ddbDocClient = DynamoDBDocument.from(client); // client is DynamoDB client
+    const ddbDocClient: DynamoDBDocument = DynamoDBDocument.from(new DynamoDB({}));
     const output: GetCommandOutput = await ddbDocClient.get(
       { TableName: this.tableName, Key: { [this.key_attr]: idempotencyKey } 
       }
     );
 
-    return new IdempotencyRecord(output.Item?.[this.key_attr], output.Item?.[this.status_attr], output.Item?.[this.expiry_attr], output.Item?.[this.in_progress_expiry_attr], undefined, undefined);
+    return new IdempotencyRecord(output.Item?.[this.key_attr], output.Item?.[this.status_attr], output.Item?.[this.expiry_attr], output.Item?.[this.in_progress_expiry_attr], output.Item?.[this.data_attr], undefined);
   }
   protected async _putRecord(_record: IdempotencyRecord): Promise<void> {}
   protected async _updateRecord(): Promise<void> {}

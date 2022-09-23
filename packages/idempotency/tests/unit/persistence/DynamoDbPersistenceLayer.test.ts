ffb2c22c74fee15346e168afc6031f1c0ecbf758
Phoenix Campos <phoenix_campos@vanguard.com>
2022-09-23 16:43:54 -0400

    feat: added tests for get record

--- packages/idempotency/tests/unit/persistence/DynamoDbPersistenceLayer.test.ts
+++ packages/idempotency/tests/unit/persistence/DynamoDbPersistenceLayer.test.ts
@@ -1,8 +1,7 @@
-import { DynamoDB, DynamoDBServiceException } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocument, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocument, GetCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 import { mockClient } from 'aws-sdk-client-mock';
 import 'aws-sdk-client-mock-jest';
-import { IdempotencyItemAlreadyExistsError } from '../../../src/Exceptions';
+import { IdempotencyItemNotFoundError } from '../../../src/Exceptions';
 import { DynamoDBPersistenceLayer } from '../../../src/persistence/DynamoDbPersistenceLayer';
 import { IdempotencyRecord } from '../../../src/persistence/IdempotencyRecord';
 import { IdempotencyRecordStatus } from '../../../src/types/IdempotencyRecordStatus';
@@ -56,6 +55,73 @@ describe('Class: DynamoDbPersistenceLayer', () => {
         ExpressionAttributeValues: { ':now': currentDate, ':inprogress': IdempotencyRecordStatus.INPROGRESS },
         ConditionExpression: 'attribute_not_exists(#id) OR #expiry < :now OR NOT #status = :inprogress'
       });
+    });
+  });
+
+  describe('Method: _getRecord', () => {
+    test('when called with a record whose key exists, it gets record', async () => {
+      class TestDynamoPersistenceLayer extends DynamoDBPersistenceLayer {
+        public _getRecord(idempotencyKey: string): Promise<IdempotencyRecord> {
+          return super._getRecord(idempotencyKey);
+        }
+      }
+
+      // Prepare
+      const tableName = 'tableName';
+      const persistenceLayer = new TestDynamoPersistenceLayer(tableName);
+
+      const key = 'key';
+
+      const status = IdempotencyRecordStatus.INPROGRESS;
+      const expiryTimestamp = 10;
+      const inProgressExpiryTimestamp = 10;
+      const responseData = {};
+      const dynamoClient = mockClient(DynamoDBDocument).on(GetCommand).resolves({ Item: { id: key, status, 'expiration': expiryTimestamp, 'in_progress_expiry_attr': inProgressExpiryTimestamp, data: responseData } });
+      jest.spyOn(Date, 'now').mockReturnValue(0);
+
+      // Act
+      const record: IdempotencyRecord = await persistenceLayer._getRecord(key);
+
+      // Assess
+      expect(dynamoClient).toReceiveCommandWith(GetCommand, {
+        TableName: tableName, Key: { id: key }
+      });
+      expect(record.getStatus()).toEqual(IdempotencyRecordStatus.INPROGRESS);
+      expect(record.idempotencyKey).toEqual(key);
+      expect(record.inProgressExpiryTimestamp).toEqual(inProgressExpiryTimestamp);
+      expect(record.responseData).toEqual(responseData);
+      expect(record.expiryTimestamp).toEqual(expiryTimestamp);
+    });
+
+    test('when called with a record whose key does not exist, it throws error', async () => {
+      class TestDynamoPersistenceLayer extends DynamoDBPersistenceLayer {
+        public _getRecord(idempotencyKey: string): Promise<IdempotencyRecord> {
+          return super._getRecord(idempotencyKey);
+        }
+      }
+  
+      // Prepare
+      const tableName = 'tableName';
+      const persistenceLayer = new TestDynamoPersistenceLayer(tableName);
+  
+      const key = 'key';
+
+      const dynamoClient = mockClient(DynamoDBDocument).on(GetCommand).resolves({ Item: undefined });
+      jest.spyOn(Date, 'now').mockReturnValue(0);
+  
+      // Act
+      let error: unknown;
+      try {
+        await persistenceLayer._getRecord(key);
+      } catch (e){
+        error = e;
+      }
+  
+      // Assess
+      expect(dynamoClient).toReceiveCommandWith(GetCommand, {
+        TableName: tableName, Key: { id: key }
+      });
+      expect(error).toBeInstanceOf(IdempotencyItemNotFoundError);
     });
   });
 });

9a6009a9a65f405e980d71caccae54e2ffb702e3
Andrea Amorosi <dreamorosi@gmail.com>
2022-11-10 15:59:16 +0100

    fix: log index in sampleRate e2e test

--- packages/commons/tests/utils/InvocationLogs.ts
+++ packages/commons/tests/utils/InvocationLogs.ts
@@ -69,7 +69,7 @@ export class InvocationLogs {
    * @returns Array of function logs
    */
   public getFunctionLogs(levelToFilter?: LEVEL): string[] {
-    let filteredLogs = this.logs.slice(1, -2);
+    let filteredLogs = this.logs.slice(1, this.getIdxOfLogThatStartsWith('END RequestId:'));
 
     if (levelToFilter) {
       filteredLogs = filteredLogs.filter((log) => {
@@ -86,6 +86,18 @@ export class InvocationLogs {
     }
 
     return filteredLogs;
+  }
+
+  /**
+   * Return the index of the log entry that starts with a given prefix
+   * @param prefix 
+   * @returns The index of the log entry with that prefix
+   */
+  public getIdxOfLogThatStartsWith(prefix: string): number {
+    const foundLog = this.logs.find((log: string) => log.startsWith(prefix));
+    if (!foundLog) throw new Error(`Unable to find log with prefix ${prefix}`);
+
+    return this.logs.indexOf(foundLog);
   }
 
   /**

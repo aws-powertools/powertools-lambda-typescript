7c020872c22d6d45d7f401669665e9db805c51a2
Andrea Amorosi <dreamorosi@gmail.com>
2022-11-11 20:16:06 +0100

    chore: revert changes to PersistenceLayerInterface

--- packages/idempotency/src/persistence/PersistenceLayerInterface.ts
+++ packages/idempotency/src/persistence/PersistenceLayerInterface.ts
@@ -1,6 +1,8 @@
 import { IdempotencyRecord } from './IdempotencyRecord';
+import type { PersistenceLayerConfigureOptions } from '../types/PersistenceLayer';
 
 interface PersistenceLayerInterface {
+  configure(options?: PersistenceLayerConfigureOptions): void
   saveInProgress(data: unknown): Promise<void>
   saveSuccess(data: unknown, result: unknown): Promise<void>
   deleteRecord(data: unknown): Promise<void>

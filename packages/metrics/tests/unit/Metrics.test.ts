6ca800c6574e8a256d184b5b2c4afdef55717ac4
Connor Kirkpatrick <ckp@amazon.com>
2022-11-14 14:42:24 +0000

    test(metrics): update metric tests to account for default service dim
    The `service_name` is now set by default. This means it is always
    present in metric dimensions. This commit updates the tests to account
    for this change

--- packages/metrics/tests/unit/Metrics.test.ts
+++ packages/metrics/tests/unit/Metrics.test.ts
@@ -76,7 +76,7 @@ describe('Class: Metrics', () => {
       metrics.addDimension(additionalDimension.name, additionalDimension.value);
       const loggedData = metrics.serializeMetrics();
 
-      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(1);
+      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
       expect(loggedData[additionalDimension.name]).toEqual(additionalDimension.value);
     });
 
@@ -104,15 +104,15 @@ describe('Class: Metrics', () => {
 
       expect(console.log).toBeCalledTimes(2);
       expect(loggedData[0][dimensionItem.name]).toEqual(dimensionItem.value);
-      expect(loggedData[0]._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(1);
+      expect(loggedData[0]._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
       expect(loggedData[1][dimensionItem.name]).toBeUndefined();
-      expect(loggedData[1]._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(0);
+      expect(loggedData[1]._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(1);
     });
 
     test('Adding more than max dimensions should throw error', () => {
       expect.assertions(1);
       const metrics = new Metrics();
-      for (let x = 0; x < MAX_DIMENSION_COUNT; x++) {
+      for (let x = 1; x < MAX_DIMENSION_COUNT; x++) {
         metrics.addDimension(`Dimension-${x}`, `value-${x}`);
       }
       try {
@@ -130,7 +130,7 @@ describe('Class: Metrics', () => {
       metrics.addDimensions(additionalDimensions);
       const loggedData = metrics.serializeMetrics();
 
-      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
+      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(3);
       Object.keys(additionalDimensions).forEach((key) => {
         expect(loggedData[key]).toEqual(additionalDimensions[key]);
       });
@@ -249,7 +249,7 @@ describe('Class: Metrics', () => {
           metrics.addMetric('test_name', MetricUnits.Seconds, 10);
           metrics.addDimension(additionalDimension.name, additionalDimension.value);
           const loggedData = metrics.serializeMetrics();
-          expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
+          expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(3);
           expect(loggedData[additionalDimension.name]).toEqual(additionalDimension.value);
           metrics.clearDimensions();
         }
@@ -259,7 +259,7 @@ describe('Class: Metrics', () => {
       const loggedData = JSON.parse(consoleSpy.mock.calls[0][0]);
 
       expect(console.log).toBeCalledTimes(1);
-      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(1);
+      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
       expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0]).toContain('default');
       expect(loggedData.default).toContain('defaultValue');
     });
@@ -661,7 +661,7 @@ describe('Class: Metrics', () => {
           metrics.addMetric('test_name', MetricUnits.Seconds, 10);
           metrics.addDimension(additionalDimension.name, additionalDimension.value);
           const loggedData = metrics.serializeMetrics();
-          expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
+          expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(3);
           expect(loggedData[additionalDimension.name]).toEqual(additionalDimension.value);
           metrics.clearDimensions();
 
@@ -673,7 +673,7 @@ describe('Class: Metrics', () => {
       const loggedData = JSON.parse(consoleSpy.mock.calls[0][0]);
 
       expect(console.log).toBeCalledTimes(1);
-      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(1);
+      expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0].length).toEqual(2);
       expect(loggedData._aws.CloudWatchMetrics[0].Dimensions[0]).toContain('default');
       expect(loggedData.default).toContain('defaultValue');
     });

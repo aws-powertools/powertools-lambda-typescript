44af8bc1035feeeb22ca0742707b17973cc7fb62
Andrea Amorosi <dreamorosi@gmail.com>
2022-12-03 18:27:59 -0800

    feat: base get

--- packages/parameters/src/SSMProvider.ts
+++ packages/parameters/src/SSMProvider.ts
@@ -2,29 +2,12 @@ import { BaseProvider } from './BaseProvider';
 import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
 import type { SSMClientConfig, GetParameterCommandInput } from '@aws-sdk/client-ssm';
 
-interface SSMProviderOptionsBase {
-  awsSdkV3Client?: SSMClient
-  sdkConfig?: SSMClientConfig
-}
-
-interface SSMOne extends SSMProviderOptionsBase {
-  awsSdkV3Client?: SSMClient
-  sdkConfig?: never
-}
-
-interface SSMTwo extends SSMProviderOptionsBase {
-  awsSdkV3Client?: never
-  sdkConfig?: SSMClientConfig
-}
-
-type SSMProviderOptions = SSMOne | SSMTwo;
-
 class SSMProvider extends BaseProvider {
   public client: SSMClient;
 
-  public constructor(options: SSMProviderOptions = {}) {
+  public constructor(config: SSMClientConfig = {}) {
     super();
-    this.client = new SSMClient(options);
+    this.client = new SSMClient(config);
   }
 
   protected async _get(name: string, sdkOptions?: Partial<GetParameterCommandInput>): Promise<string | undefined> {
@@ -39,7 +22,7 @@ class SSMProvider extends BaseProvider {
     return result.Parameter?.Value;
   }
 
-  protected async _getMultiple(_path: string): Promise<Record<string, string | undefined>> {
+  protected _getMultiple(_path: string): Promise<Record<string, string | undefined>> {
     throw Error('Not implemented.');
   }
 }
--- packages/parameters/tests/unit/SSMProvider.test.ts
+++ packages/parameters/tests/unit/SSMProvider.test.ts
@@ -16,38 +16,58 @@ describe('Class: SSMProvider', () => {
 
   describe('Method: get', () => {
 
-    test('when called without sdkOptions, it gets the parameter using the name only', () => {
+    test('when called without sdkOptions, it gets the parameter using the name and with no decryption', async () => {
 
       // Prepare
       const provider = new SSMProvider();
-      const client = mockClient(SSMClient).on(GetParameterCommand).resolves({});
       const parameterName = 'foo';
-
-      // Act
-      provider.get(parameterName);
-
-      // Assess
-      expect(client).toReceiveCommandWith(GetParameterCommand, {
-        Name: parameterName
+      const parameterValue = 'foo';
+      const client = mockClient(SSMClient).on(GetParameterCommand).resolves({
+        Parameter: {
+          Value: parameterValue,
+        },
       });
 
-    });
-    
-    test('when called with sdkOptions, it gets the parameter using the parameters', () => {
-
-      // Prepare
-      const provider = new SSMProvider();
-      const client = mockClient(SSMClient).on(GetParameterCommand).resolves({});
-      const parameterName = 'foo';
-
       // Act
-      provider.get(parameterName, { sdkOptions: { } });
+      const value = await provider.get(parameterName);
 
       // Assess
       expect(client).toReceiveCommandWith(GetParameterCommand, {
         Name: parameterName,
-        
       });
+      expect(value).toBe(parameterValue);
+
+    });
+    
+    test('when called with sdkOptions, it gets the parameter using the parameters', async () => {
+
+      // Prepare
+      const provider = new SSMProvider();
+      const client = mockClient(SSMClient).on(GetParameterCommand).resolves({});
+      const parameterName = 'foo';
+
+      // Act
+      provider.get(parameterName, { sdkOptions: { WithDecryption: true } });
+
+      // Assess
+      expect(client).toReceiveCommandWith(GetParameterCommand, {
+        Name: parameterName,
+        WithDecryption: true,
+      });
+
+    });
+
+  });
+
+  describe('Method: _getMultiple', () => {
+
+    test('when called throws', async () => {
+
+      // Prepare
+      const provider = new SSMProvider();
+
+      // Act / Assess
+      expect(provider.getMultiple('foo')).rejects.toThrow('Not implemented.');
 
     });
 

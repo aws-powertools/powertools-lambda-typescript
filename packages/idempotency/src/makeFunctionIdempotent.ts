68ce9a265301a5546185505400e746ca51a2106f
KevenFuentes9 <keven_fuentes@vanguard.com>
2022-12-09 15:49:14 -0500

    chore: move and enhance comment on question

--- packages/idempotency/src/makeFunctionIdempotent.ts
+++ packages/idempotency/src/makeFunctionIdempotent.ts
@@ -7,8 +7,12 @@ const makeFunctionIdempotent = function<U>(
   fn: AnyFunctionWithRecord<U>,
   options: IdempotencyOptions
 ): AnyIdempotencyFunction<U>{
+  /*
+    TODO: need to find out what we are doing to par down the "payload" and what we may enforce as far as parameters
+    have an open question out: https://github.com/awslabs/aws-lambda-powertools-typescript/issues/447#issuecomment-1344622163
+  */
+
   const wrappedFunction: AnyIdempotencyFunction<U> = function(record: Record<string, any>): Promise<U> {
-    //TODO: need to find out what we are doing to par down the "payload" - have an open question out: https://github.com/awslabs/aws-lambda-powertools-typescript/issues/447#issuecomment-1344622163
     const idempotencyHandler: IdempotencyHandler<U> = new IdempotencyHandler<U>(fn,record , options, [record]);
     
     return idempotencyHandler.process_idempotency();

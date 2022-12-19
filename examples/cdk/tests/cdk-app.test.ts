283110eebf75750eb196bb8356ce959895584919
Andrea Amorosi <dreamorosi@gmail.com>
2022-12-19 17:49:24 +0100

    tests: update unit tests

--- examples/cdk/tests/cdk-app.test.ts
+++ examples/cdk/tests/cdk-app.test.ts
@@ -5,5 +5,5 @@ import * as CdkApp from '../src/example-stack';
 test('CDK code synthesize', () => {
   const app = new cdk.App();
   const stack = new CdkApp.CdkAppStack(app, 'MyTestStack');
-  Template.fromStack(stack).resourceCountIs('AWS::Lambda::Function', 14);
+  Template.fromStack(stack).resourceCountIs('AWS::Lambda::Function', 4); // The stack has 4 functions: 3 for the example, and 1 for the log retention that is deployed by CDK
 });

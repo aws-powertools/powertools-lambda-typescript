63eeab4c87cec7a48c689ae2368f2588283bc1af
Andrea Amorosi <dreamorosi@gmail.com>
2022-12-29 15:39:56 +0100

    refactor(parameters): BaseProvider support for Uint8Array (#1205)
    * refactor: added Uint8Array to BaseProvider
    
    * chore: manually bumped utility version

--- packages/parameters/src/ExpirableValue.ts
+++ packages/parameters/src/ExpirableValue.ts
@@ -2,9 +2,9 @@ import type { ExpirableValueInterface } from './types';
 
 class ExpirableValue implements ExpirableValueInterface {
   public ttl: number;
-  public value: string | Record<string, unknown>;
+  public value: string | Uint8Array | Record<string, unknown>;
 
-  public constructor(value: string | Record<string, unknown>, maxAge: number) {
+  public constructor(value: string | Uint8Array | Record<string, unknown>, maxAge: number) {
     this.value = value;
     const timeNow = new Date();
     this.ttl = timeNow.setSeconds(timeNow.getSeconds() + maxAge);
@@ -15,6 +15,6 @@ class ExpirableValue implements ExpirableValueInterface {
   }
 }
 
-export { 
+export {
   ExpirableValue
 };

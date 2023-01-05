2e3c5b28e78c9839fd8d973c4151b2a73b1609ca
Andrea Amorosi <dreamorosi@gmail.com>
2023-01-05 16:19:48 +0100

    refactor: readability

--- packages/parameters/src/secrets/SecretsProvider.ts
+++ packages/parameters/src/secrets/SecretsProvider.ts
@@ -1,7 +1,13 @@
 import { BaseProvider } from '../BaseProvider';
-import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand
+} from '@aws-sdk/client-secrets-manager';
 import type { GetSecretValueCommandInput } from '@aws-sdk/client-secrets-manager';
-import type { SecretsProviderOptions, SecretsGetOptionsInterface } from 'types/SecretsProvider';
+import type {
+  SecretsProviderOptions,
+  SecretsGetOptionsInterface
+} from '../types/SecretsProvider';
 
 class SecretsProvider extends BaseProvider {
   public client: SecretsManagerClient;
@@ -13,18 +19,21 @@ class SecretsProvider extends BaseProvider {
     this.client = new SecretsManagerClient(clientConfig);
   }
 
-  public async get(name: string, options?: SecretsGetOptionsInterface): Promise<undefined | string | Uint8Array | Record<string, unknown>> {
+  public async get(
+    name: string,
+    options?: SecretsGetOptionsInterface
+  ): Promise<undefined | string | Uint8Array | Record<string, unknown>> {
     return super.get(name, options);
   }
 
-  protected async _get(name: string, options?: SecretsGetOptionsInterface): Promise<string | Uint8Array | undefined> {
+  protected async _get(
+    name: string,
+    options?: SecretsGetOptionsInterface
+  ): Promise<string | Uint8Array | undefined> {
     const sdkOptions: GetSecretValueCommandInput = {
+      ...(options?.sdkOptions || {}),
       SecretId: name,
     };
-    if (options?.sdkOptions) {
-      this.removeNonOverridableOptions(options.sdkOptions as GetSecretValueCommandInput);
-      Object.assign(sdkOptions, options.sdkOptions);
-    }
 
     const result = await this.client.send(new GetSecretValueCommand(sdkOptions));
 
@@ -36,20 +45,12 @@ class SecretsProvider extends BaseProvider {
   /**
    * Retrieving multiple parameter values is not supported with AWS Secrets Manager.
    */
-  protected async _getMultiple(_path: string, _options?: unknown): Promise<Record<string, string | undefined>> {
+  protected async _getMultiple(
+    _path: string,
+    _options?: unknown
+  ): Promise<Record<string, string | undefined>> {
     throw new Error('Method not implemented.');
-  }
-
-  /**
-   * Explicit arguments passed to the constructor will take precedence over ones passed to the method.
-   * For users who consume the library with TypeScript, this will be enforced by the type system. However,
-   * for JavaScript users, we need to manually delete the properties that are not allowed to be overridden.
-   */
-  protected removeNonOverridableOptions(options: GetSecretValueCommandInput): void {
-    if (options.hasOwnProperty('SecretId')) {
-      delete options.SecretId;
-    }
-  }
+  } 
 }
 
 export {
--- packages/parameters/tests/unit/SecretsProvider.test.ts
+++ packages/parameters/tests/unit/SecretsProvider.test.ts
@@ -80,6 +80,29 @@ describe('Class: SecretsProvider', () => {
 
     });
 
+    test('when called with sdkOptions that override arguments passed to the method, it gets the secret using the arguments', async () => {
+
+      // Prepare
+      const provider = new SecretsProvider();
+      const secretName = 'foo';
+      client.on(GetSecretValueCommand).resolves({
+        SecretString: 'bar',
+      });
+
+      // Act
+      await provider.get(secretName, {
+        sdkOptions: {
+          SecretId: 'test-secret',
+        } as unknown as GetSecretValueCommandInput,
+      });
+
+      // Assess
+      expect(client).toReceiveCommandWith(GetSecretValueCommand, {
+        SecretId: secretName,
+      });
+
+    });
+
   });
 
   describe('Method: _getMultiple', () => {
@@ -91,35 +114,6 @@ describe('Class: SecretsProvider', () => {
       
       // Act & Assess
       await expect(provider.getMultiple('foo')).rejects.toThrow('Method not implemented.');
-
-    });
-
-  });
-
-  describe('Method: removeNonOverridableOptions', () => {
-
-    class DummyProvider extends SecretsProvider {
-      public removeNonOverridableOptions(options: GetSecretValueCommandInput): void {
-        super.removeNonOverridableOptions(options);
-      }
-    }
-
-    test('when called with a valid GetSecretValueCommandInput, it removes the non-overridable options', () => {
-
-      // Prepare
-      const provider = new DummyProvider();
-      const options: GetSecretValueCommandInput = {
-        SecretId: 'test-secret',
-        VersionId: 'test-version',
-      };
-
-      // Act
-      provider.removeNonOverridableOptions(options);
-
-      // Assess
-      expect(options).toEqual({
-        VersionId: 'test-version',
-      });
 
     });
 

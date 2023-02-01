b86860490ec3b34d9e2c5c954794f144a1a84547
Alexander Melnyk <amelnyk@amazon.com>
2023-02-01 10:52:05 +0100

    add provider to signature

--- packages/parameters/tests/e2e/secretsProvider.class.test.functionCode.ts
+++ packages/parameters/tests/e2e/secretsProvider.class.test.functionCode.ts
@@ -4,7 +4,7 @@ import { TinyLogger } from '../helpers/tinyLogger';
 import { SecretsGetOptionsInterface } from '../../lib/types';
 
 const logger = new TinyLogger();
-const provider = new SecretsProvider();
+const defaultProvider = new SecretsProvider();
 
 const secretNamePlain = process.env.SECRET_NAME_PLAIN || '';
 const secretNameObject = process.env.SECRET_NAME_OBJECT || '';
@@ -12,21 +12,16 @@ const secretNameBinary = process.env.SECRET_NAME_BINARY || '';
 const secretNameObjectWithSuffix = process.env.SECRET_NAME_OBJECT_WITH_SUFFIX || '';
 const secretNameBinaryWithSuffix = process.env.SECRET_NAME_BINARY_WITH_SUFFIX || '';
 
-const _call_get = async (paramName: string, testName: string, options?: SecretsGetOptionsInterface) : Promise<void> => {
+const _call_get = async (paramName: string, testName: string, options?: SecretsGetOptionsInterface, provider?: SecretsProvider,): Promise<void> => {
   try {
-    if (!options) {
-      const parameterValue = await provider.get(paramName);
-      logger.log({
-        test: testName,
-        value: parameterValue
-      });
-    } else {
-      const parameterValue = await provider.get(paramName, options);
-      logger.log({
-        test: testName,
-        value: parameterValue
-      });
-    }
+    // we might get a provider with specific sdk options, otherwise fallback to default
+    const currentProvider = provider ? provider : defaultProvider;
+
+    const parameterValue = await currentProvider.get(paramName, options);
+    logger.log({
+      test: testName,
+      value: parameterValue
+    });
   } catch (err) {
     logger.log({
       test: testName,
@@ -51,6 +46,8 @@ export const handler = async (_event: unknown, _context: Context): Promise<void>
 
   // Test 5 get single param with transform auto binary
   await _call_get(secretNameBinaryWithSuffix, 'get-transform-auto-binary', { transform: 'auto' });
+
+  // TOOD: add more tests once we can pass sdk configuration to the provider
 
 };
 

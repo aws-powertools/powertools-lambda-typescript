64508c737dc64b5b260902e5b551d1cd40b3c41e
Andrea Amorosi <dreamorosi@gmail.com>
2023-02-09 13:14:38 +0100

    docs: updated typedoc/parameters config

--- typedoc.js
+++ typedoc.js
@@ -3,6 +3,7 @@ module.exports = {
   exclude: [ '**/node_modules/**', '**/*.test.ts', '**/*.json' ],
   name: 'aws-lambda-powertools-typescript',
   excludePrivate: true,
+  excludeInternal: true,
   entryPointStrategy: 'packages',
   readme: './README.md',
 };

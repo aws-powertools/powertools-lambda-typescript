0b05cb000a03a600e5c88f50ba0851ebd7c73738
Andrea Amorosi <dreamorosi@gmail.com>
2023-02-27 15:57:36 +0100

    tests: basicFeatures

--- packages/logger/tests/e2e/constants.ts
+++ packages/logger/tests/e2e/constants.ts
@@ -4,3 +4,4 @@ export const TEST_CASE_TIMEOUT = ONE_MINUTE;
 export const SETUP_TIMEOUT = 5 * ONE_MINUTE;
 export const TEARDOWN_TIMEOUT = 5 * ONE_MINUTE;
 export const STACK_OUTPUT_LOG_GROUP = 'LogGroupName';
+export const XRAY_TRACE_ID_REGEX = /^1-[0-9a-f]{8}-[0-9a-f]{24}$/;

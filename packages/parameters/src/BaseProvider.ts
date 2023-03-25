ec49023c44c3873ba5396a45ee9b2a8ee031e84b
Andrea Amorosi <dreamorosi@gmail.com>
2023-03-25 20:18:31 +0100

    feat(parameters): add `clearCaches` function (#1382)

--- packages/parameters/src/BaseProvider.ts
+++ packages/parameters/src/BaseProvider.ts
@@ -220,9 +220,21 @@ const transformValues = (value: Record<string, string | undefined>, transform: T
   return transformedValues;
 };
 
+/**
+ * Utility function to clear all the caches of the default providers.
+ * 
+ * This is useful when you want to clear the cache of all the providers at once, for example during testing.
+ */
+const clearCaches = (): void => {
+  for (const provider of Object.values(DEFAULT_PROVIDERS)) {
+    provider.clearCache();
+  }
+};
+
 export {
   BaseProvider,
   ExpirableValue,
   transformValue,
   DEFAULT_PROVIDERS,
+  clearCaches,
 };

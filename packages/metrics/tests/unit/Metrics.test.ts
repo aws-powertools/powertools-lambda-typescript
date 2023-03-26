37adcabb6ad8cab3ac787b73dda0d6163aa2dced
Arnab Rahman <arnabrahman@hotmail.com>
2023-03-26 15:01:38 +0600

    test: store metrics

--- packages/metrics/tests/unit/Metrics.test.ts
+++ packages/metrics/tests/unit/Metrics.test.ts
@@ -4,16 +4,38 @@
  * @group unit/metrics/class
  */
 
-const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+import { MetricResolution, MetricUnits, Metrics } from '../../src/';
 
 describe('Class: Metrics', () => {
   const ENVIRONMENT_VARIABLES = process.env;
 
-  beforeEach(() => {
-    consoleSpy.mockClear();
-  });
-
   beforeAll(() => {
     process.env = { ...ENVIRONMENT_VARIABLES };
+  });
+
+  describe('Method: addMetric', () => {
+    
+    test('when called, it should store metrics', () => {
+      
+      //Prepare
+      const metrics = new Metrics();
+      const metricName = 'test_metric';
+
+      //Act
+      metrics.addMetric(metricName, MetricUnits.Count, 1, MetricResolution.High);
+
+      // Assess
+      expect(metrics).toEqual(expect.objectContaining({
+        storedMetrics: {
+          [metricName]: {
+            name: metricName,
+            resolution: MetricResolution.High,
+            unit: MetricUnits.Count,
+            value: 1
+          }
+        },
+      }));
+    });
+
   });
 });

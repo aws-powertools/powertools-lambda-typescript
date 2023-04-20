80e4c3b027a1d032fb309aa8b8bf60417feff009
Arnab Rahman <arnabrahman@hotmail.com>
2023-04-20 12:32:34 +0600

    refactor: function export for metricsUtils

--- packages/metrics/tests/helpers/metricsUtils.ts
+++ packages/metrics/tests/helpers/metricsUtils.ts
@@ -5,7 +5,7 @@ import { ExtraOptions, MetricUnits } from '../../src/types';
 import { Context, Handler } from 'aws-lambda';
 import { LambdaInterface } from '@aws-lambda-powertools/commons';
 
-export const getMetrics = async (cloudWatchClient: CloudWatch, namespace: string, metric: string, expectedMetrics: number): Promise<CloudWatch.ListMetricsOutput> => {
+const getMetrics = async (cloudWatchClient: CloudWatch, namespace: string, metric: string, expectedMetrics: number): Promise<CloudWatch.ListMetricsOutput> => {
   const retryOptions = { retries: 20, minTimeout: 5_000, maxTimeout: 10_000, factor: 1.25 };
 
   return promiseRetry(async (retry: (err?: Error) => never, _: number) => {
@@ -25,7 +25,7 @@ export const getMetrics = async (cloudWatchClient: CloudWatch, namespace: string
   }, retryOptions);
 };
 
-export const setupDecoratorLambdaHandler = (metrics: Metrics, options: ExtraOptions = {}): Handler => {
+const setupDecoratorLambdaHandler = (metrics: Metrics, options: ExtraOptions = {}): Handler => {
     
   class LambdaFunction implements LambdaInterface {
     @metrics.logMetrics(options)
@@ -42,5 +42,10 @@ export const setupDecoratorLambdaHandler = (metrics: Metrics, options: ExtraOpti
   const handler = handlerClass.handler.bind(handlerClass);
   
   return handler;
+};
+
+export {
+  getMetrics,
+  setupDecoratorLambdaHandler
 };
 

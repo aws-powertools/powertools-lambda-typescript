b7c88d48e9c02886dca812b685b348bece36725c
Andrea Amorosi <dreamorosi@gmail.com>
2023-06-15 22:50:02 +0200

    chore: clean up package.json files

--- .github/scripts/release_patch_package_json.js
+++ .github/scripts/release_patch_package_json.js
@@ -1,13 +1,18 @@
 const { readFileSync, writeFileSync } = require('node:fs');
+const { join, resolve } = require('node:path');
 
-const outDir = './lib';
-const betaPackages = [
-  '@aws-lambda-powertools/parameters',
-];
+if (process.argv.length < 3) {
+  console.error('Usage: node release_patch_package_json.js <package_path>\n');
+  process.exit(1);
+}
+const basePath = resolve(process.argv[2]);
+const packageJsonPath = join(basePath, 'package.json');
+const alphaPackages = ['@aws-lambda-powertools/idempotency'];
+const betaPackages = ['@aws-lambda-powertools/parameters'];
 
 /**
  * This script is used to create a new package.json file for the tarball that will be published to npm.
- * 
+ *
  * The original package.json file is read and the following fields are extracted:
  * - name
  * - version
@@ -20,9 +25,9 @@ const betaPackages = [
  * - keywords
  * - dependencies
  * - devDependencies
- * 
+ *
  * For beta packages, the version number is updated to include a beta suffix.
- * 
+ *
  * The new package.json file is written to the lib folder, which is the folder that will be packed and published to npm.
  * For beta packages, the original package.json file is also temporarily updated with the new beta version number so that
  * the version number in the registry is correct and matches the tarball.
@@ -30,7 +35,7 @@ const betaPackages = [
 (() => {
   try {
     // Read the original package.json file
-    const pkgJson = JSON.parse(readFileSync('./package.json', 'utf8'))
+    const pkgJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
     // Extract the fields we want to keep
     const {
       name,
@@ -43,12 +48,17 @@ const betaPackages = [
       bugs,
       keywords,
       dependencies,
-      devDependencies,
+      exports,
+      typesVersions,
+      main,
+      types,
     } = pkgJson;
 
     let version = originalVersion;
-    // Add a beta suffix to the version
-    if (betaPackages.includes(name)) {
+    // If the package is an alpha or beta package, update the version number to include a suffix
+    if (alphaPackages.includes(name)) {
+      version = `${version}-alpha`;
+    } else if (betaPackages.includes(name)) {
       version = `${version}-beta`;
     }
 
@@ -64,20 +74,23 @@ const betaPackages = [
       bugs,
       keywords,
       dependencies,
-      devDependencies,
-      main: './index.js',
-      types: './index.d.ts',
     };
 
     // Write the new package.json file inside the folder that will be packed
-    writeFileSync(`${outDir}/package.json`, JSON.stringify(newPkgJson, null, 2));
+    writeFileSync(
+      `${outDir}/package.json`,
+      JSON.stringify(newPkgJson, null, 2)
+    );
 
     if (betaPackages.includes(name)) {
       // Temporarily update the original package.json file with the new beta version.
       // This version number will be picked up during the `npm publish` step, so that
       // the version number in the registry is correct and matches the tarball.
       // The original package.json file will be restored by lerna after the publish step.
-      writeFileSync('package.json', JSON.stringify({ ...pkgJson, version }, null, 2));
+      writeFileSync(
+        'package.json',
+        JSON.stringify({ ...pkgJson, version }, null, 2)
+      );
     }
   } catch (err) {
     throw err;

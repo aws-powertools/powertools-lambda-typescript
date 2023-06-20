cbddc6c05a3784ffd3f039b1ca7c255638ef8863
Andrea Amorosi <dreamorosi@gmail.com>
2023-06-20 14:41:18 +0200

    chore: clarify instrument clients

--- docs/snippets/tracer/captureAWSAll.ts
+++ docs/snippets/tracer/captureAWSAll.ts
@@ -2,4 +2,10 @@ import { Tracer } from '@aws-lambda-powertools/tracer';
 import AWS from 'aws-sdk';
 
 const tracer = new Tracer({ serviceName: 'serverlessAirline' });
+
+// Instrument all AWS SDK clients created from this point onwards
 tracer.captureAWS(AWS);
+
+// Create a new client which will be automatically instrumented
+const client = new AWS.SecretsManager();
+export default client;

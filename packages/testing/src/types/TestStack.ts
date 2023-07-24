2f251f2d9aa35bc9d5f985172c2dc832e28cae95
Andrea Amorosi <dreamorosi@gmail.com>
2023-07-24 14:59:29 +0200

    chore: add comments on types

--- packages/testing/src/types/TestStack.ts
+++ packages/testing/src/types/TestStack.ts
@@ -5,23 +5,38 @@ import type {
   SsmStringOptions,
 } from './factories';
 
+/**
+ * Options to add a NodejsFunction to the test stack.
+ */
 type AddFunctionOptions = Omit<
   NodejsFunctionOptions,
   'stack' | 'resourceId' | 'runtime'
 >;
 
+/**
+ * Options to add a DynamoDB table to the test stack.
+ */
 type AddDynamoDBTableOptions = Omit<
   DynamoDBTableOptions,
   'stack' | 'resourceId'
 >;
 
+/**
+ * Options to add a SSM Secure String to the test stack.
+ */
 type AddSsmSecureStringOptions = Omit<
   SsmSecureStringOptions,
   'stack' | 'resourceId'
 >;
 
+/**
+ * Options to add a SSM String to the test stack.
+ */
 type AddSsmStringOptions = Omit<SsmStringOptions, 'stack' | 'resourceId'>;
 
+/**
+ * Options to add a test case to the test stack.
+ */
 type AddTestCaseOptions = {
   testCaseName: string;
   function: AddFunctionOptions;

8b39cd651f109784391f6cc9ef4a214c09dd6deb
Alexander Melnyk <amelnyk@amazon.com>
2023-07-24 15:15:47 +0200

    swtich to randomUUID

--- examples/cdk/functions/common/getUuid.ts
+++ examples/cdk/functions/common/getUuid.ts
@@ -1,6 +1,6 @@
 import { logger } from './powertools';
 import { getParameter } from '@aws-lambda-powertools/parameters/ssm';
-import { v4 } from 'uuid';
+import { randomUUID } from 'node:crypto';
 import { default as request } from 'phin';
 
 export const getUuid = async (): Promise<string> => {
@@ -9,7 +9,7 @@ export const getUuid = async (): Promise<string> => {
     // create uuid locally
     logger.warn('No uuid-api-url parameter found, creating uuid locally');
 
-    return v4();
+    return randomUUID();
   } else {
     // Request a sample random uuid from a webservice
     const res = await request<{ uuid: string }>({

88cb1ef2a47eff98b000fdb61c2b58285491ff84
Andrea Amorosi <dreamorosi@gmail.com>
2023-09-15 21:05:37 +0000

    chore: temporarily patch layers e2e

--- .github/scripts/setup_tmp_layer_files.sh
+++ .github/scripts/setup_tmp_layer_files.sh
@@ -2,12 +2,16 @@
 rm -rf tmp/nodejs
 mkdir -p tmp/nodejs
 npm run build -w packages/commons
+npm pack -w packages/commons
 mv aws-lambda-powertools-commons-*.tgz tmp/nodejs
 npm run build -w packages/logger
+npm pack -w packages/logger
 mv aws-lambda-powertools-logger-*.tgz tmp/nodejs
 npm run build -w packages/metrics
+npm pack -w packages/metrics
 mv aws-lambda-powertools-metrics-*.tgz tmp/nodejs
 npm run build -w packages/tracer
+npm pack -w packages/tracer
 mv aws-lambda-powertools-tracer-*.tgz tmp/nodejs
 cd tmp/nodejs
 npm init -y
@@ -18,5 +22,9 @@ npm i \
   aws-lambda-powertools-tracer-*.tgz
 rm -rf node_modules/@types \
   package.json \
-  package-lock.json
+  package-lock.json \
+  aws-lambda-powertools-commons-*.tgz \
+  aws-lambda-powertools-logger-*.tgz \
+  aws-lambda-powertools-metrics-*.tgz \
+  aws-lambda-powertools-tracer-*.tgz
 cd ../..

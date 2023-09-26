b14afea77d0c03100d5a41279ee41d723ed74bef
Alexander Melnyk <sha@amazon.com>
2023-09-26 22:59:25 +0200

    remove unused code, merged comments

--- packages/idempotency/src/idempotencyDecorator.ts
+++ packages/idempotency/src/idempotencyDecorator.ts
@@ -1,102 +1,11 @@
-import type { Context, Handler } from 'aws-lambda';
-import {
-  AnyFunction,
-  IdempotencyLambdaHandlerOptions,
-  ItempotentFunctionOptions,
-} from './types';
-import { IdempotencyHandler } from './IdempotencyHandler';
-import { IdempotencyConfig } from './IdempotencyConfig';
+import { AnyFunction, ItempotentFunctionOptions } from './types';
 import { makeIdempotent } from './makeIdempotent';
-
-const isContext = (arg: unknown): arg is Context => {
-  return (
-    arg !== undefined &&
-    arg !== null &&
-    typeof arg === 'object' &&
-    'getRemainingTimeInMillis' in arg
-  );
-};
-
-const isFnHandler = (
-  fn: AnyFunction,
-  args: Parameters<AnyFunction>
-): fn is Handler => {
-  // get arguments of function
-  return (
-    fn !== undefined &&
-    fn !== null &&
-    typeof fn === 'function' &&
-    isContext(args[1])
-  );
-};
-
-const isOptionsWithDataIndexArgument = (
-  options: unknown
-): options is IdempotencyLambdaHandlerOptions & {
-  dataIndexArgument: number;
-} => {
-  return (
-    options !== undefined &&
-    options !== null &&
-    typeof options === 'object' &&
-    'dataIndexArgument' in options
-  );
-};
-
-const idempotent = function (
-  options: ItempotentFunctionOptions<Parameters<AnyFunction>>
-): (
-  target: unknown,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) => PropertyDescriptor {
-  return function (
-    _target: unknown,
-    _propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const childFunction = descriptor.value;
-    descriptor.value = makeIdempotent(childFunction, options);
-    descriptor.value = function (...args: Parameters<AnyFunction>) {
-      const { persistenceStore, config } = options;
-      const idempotencyConfig = config ? config : new IdempotencyConfig({});
-
-      if (!idempotencyConfig.isEnabled())
-        return childFunction.apply(this, args);
-
-      let functionPayloadtoBeHashed;
-
-      if (isFnHandler(childFunction, args)) {
-        idempotencyConfig.registerLambdaContext(args[1]);
-        functionPayloadtoBeHashed = args[0];
-      } else {
-        if (isOptionsWithDataIndexArgument(options)) {
-          functionPayloadtoBeHashed = args[options.dataIndexArgument];
-        } else {
-          functionPayloadtoBeHashed = args[0];
-        }
-      }
-
-      return new IdempotencyHandler({
-        functionToMakeIdempotent: childFunction,
-        idempotencyConfig: idempotencyConfig,
-        persistenceStore: persistenceStore,
-        functionArguments: args,
-        functionPayloadToBeHashed: functionPayloadtoBeHashed,
-      }).handle();
-    };
-
-    return descriptor;
-  };
-};
 
 /**
  * Use this decorator to make your lambda handler itempotent.
  * You need to provide a peristance layer to store the idempotency information.
  * At the moment we only support `DynamodbPersistenceLayer`.
- * > **Note**:
- * > decorators are an exeperimental feature in typescript and may change in the future.
- * > To enable decoratopr support in your project, you need to enable the `experimentalDecorators` compiler option in your tsconfig.json file.
+ *
  * @example
  * ```ts
  * import {
@@ -105,7 +14,7 @@ const idempotent = function (
  * } from '@aws-lambda-powertools/idempotency'
  *
  * class MyLambdaFunction {
- *   @idempotentLambdaHandler({ persistenceStore: new DynamoDBPersistenceLayer() })
+ *   @idempotent({ persistenceStore: new DynamoDBPersistenceLayer() })
  *   async handler(event: any, context: any) {
  *     return "Hello World";
  *   }
@@ -113,21 +22,8 @@ const idempotent = function (
  * export myLambdaHandler new MyLambdaFunction();
  * export const handler = myLambdaHandler.handler.bind(myLambdaHandler);
  * ```
- * @see {@link DynamoDBPersistenceLayer}
- * @see https://www.typescriptlang.org/docs/handbook/decorators.html
- */
-// const idempotentLambdaHandler = function (
-//   options: IdempotencyLambdaHandlerOptions
-// ): (
-//   target: unknown,
-//   propertyKey: string,
-//   descriptor: PropertyDescriptor
-// ) => PropertyDescriptor {
-//   return idempotent(options);
-// };
-/**
- * Use this decorator to make any class function idempotent.
- * Similar to the `idempotentLambdaHandler` decorator, you need to provide a persistence layer to store the idempotency information.
+ *
+ * Similar to decoratoring a handler you can use the decorator on any other function.
  * @example
  * ```ts
  * import {
@@ -143,22 +39,31 @@ const idempotent = function (
  *      }
  *    }
  *
- *  @idempotentFunction({ persistenceStore: new DynamoDBPersistenceLayer() })
+ *  @idemptent({ persistenceStore: new DynamoDBPersistenceLayer() })
  *  public async process(record: Record<stiring, unknown) {
  *     // do some processing
  *  }
- *  ```
- *  @see {@link DynamoDBPersistenceLayer}
- * @param options
+ * ```
+ * @see {@link DynamoDBPersistenceLayer}
+ * @see https://www.typescriptlang.org/docs/handbook/decorators.html
  */
-// const idempotentFunction = function (
-//   options: ItempotentFunctionOptions<Parameters<AnyFunction>>
-// ): (
-//   target: unknown,
-//   propertyKey: string,
-//   descriptor: PropertyDescriptor
-// ) => PropertyDescriptor {
-//   return idempotent(options);
-// };
+const idempotent = function (
+  options: ItempotentFunctionOptions<Parameters<AnyFunction>>
+): (
+  target: unknown,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) => PropertyDescriptor {
+  return function (
+    _target: unknown,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const childFunction = descriptor.value;
+    descriptor.value = makeIdempotent(childFunction, options);
+
+    return descriptor;
+  };
+};
 
 export { idempotent };

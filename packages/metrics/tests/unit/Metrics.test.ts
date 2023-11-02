2931c5adc4ee762fb6e1a1194f54588b031808ff
Alexander Schueren <sha@amazon.com>
2023-11-02 17:42:52 +0100

    fix(metrics): deduplicate dimensions when serialising (#1780)
    * fix: deduplicate dimensions when serializing
    
    * fix tests
    
    * remove tsbuildinfo
    
    * remove whitespace
    
    * fix gitignore again
    
    * play some sonar games
    
    * fix test

--- packages/metrics/tests/unit/Metrics.test.ts
+++ packages/metrics/tests/unit/Metrics.test.ts
@@ -1530,7 +1530,11 @@ describe('Class: Metrics', () => {
       });
 
       // Act
+<<<<<<< HEAD
       metrics.addMetric(testMetric, MetricUnit.Count, 10);
+=======
+      metrics.addMetric(testMetric, MetricUnits.Count, 10);
+>>>>>>> 9b9fc927 (fix(metrics): deduplicate dimensions when serialising (#1780))
       metrics.addDimension('foo', 'baz');
       const loggedData = metrics.serializeMetrics();
 
@@ -1549,7 +1553,11 @@ describe('Class: Metrics', () => {
               Metrics: [
                 {
                   Name: testMetric,
+<<<<<<< HEAD
                   Unit: MetricUnit.Count,
+=======
+                  Unit: MetricUnits.Count,
+>>>>>>> 9b9fc927 (fix(metrics): deduplicate dimensions when serialising (#1780))
                 },
               ],
               Namespace: TEST_NAMESPACE,

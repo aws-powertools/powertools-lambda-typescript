e330e51c07ef1e9652b49165984ae2769bb7d7e9
Alexander Schueren <sha@amazon.com>
2023-11-17 21:00:50 +0100

    moved comment, fixed path

--- packages/parser/tests/unit/schema/utils.ts
+++ packages/parser/tests/unit/schema/utils.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs';
 
 export const loadExampleEvent = (fileName: string): unknown => {
-  const event = readFileSync(`tests/events/${fileName}`, 'utf8');
+  const event = readFileSync(`./tests/events/${fileName}`, 'utf8');
 
   return JSON.parse(event);
 };

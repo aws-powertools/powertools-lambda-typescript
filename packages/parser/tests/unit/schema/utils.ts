455b329d8e0ae20fa53fcbedfaa0e99febc7e70b
Alexander Schueren <sha@amazon.com>
2023-11-17 17:45:42 +0100

    imports, remove try/catch

--- packages/parser/tests/unit/schema/utils.ts
+++ packages/parser/tests/unit/schema/utils.ts
@@ -1,11 +1,7 @@
-import * as fs from 'fs';
+import { readFileSync } from 'node:fs';
 
 export const loadExampleEvent = (fileName: string): unknown => {
-  try {
-    const event = fs.readFileSync(`tests/events/${fileName}`, 'utf8');
+  const event = readFileSync(`tests/events/${fileName}`, 'utf8');
 
-    return JSON.parse(event);
-  } catch (err) {
-    console.error(err);
-  }
+  return JSON.parse(event);
 };

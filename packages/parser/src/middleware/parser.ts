1f20c01007dc467ba206809878ff4506c0577d47
Alexander Schueren <sha@amazon.com>
2023-11-25 23:04:31 +0100

    add middy parser

--- packages/parser/src/middleware/parser.ts
+++ packages/parser/src/middleware/parser.ts
@@ -8,6 +8,34 @@ interface ParserOptions {
   envelope?: Envelope;
 }
 
+/**
+ * A middiy middleware to parse your event.
+ *
+ * @exmaple
+ * ```typescirpt
+ * import { parser } from '@aws-lambda-powertools/parser/middleware';
+ * import middy from '@middy/core';
+ * import { SQS_ENVELOPE } from '@aws-lambda-powertools/parser/envelopes;'
+ *
+ * const oderSchema = z.object({
+ *  id: z.number(),
+ *  description: z.string(),
+ *  quantity: z.number()
+ * }
+ *
+ * type Order = z.infer<typeof orderSchema>;
+ *
+ * export class handler = middy(
+ *  async(event: Order, _context: unknown): Promise<void> => {
+ *    // event is validated as sqs message envelope
+ *    // the body is unwrapped and parsed into object ready to use
+ *    // you can now use event as Order in your code
+ *  }
+ * ).use(parser({ schema: oderSchema, envelope: SQS_ENVELOPE }));
+ * ```
+ *
+ * @param options
+ */
 const parser = (options: ParserOptions): MiddlewareObj => {
   const before = (request: MiddyLikeRequest): void => {
     const { schema, envelope } = options;

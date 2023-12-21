3bafad7d8bd9481b9c3b27807488598c25bef951
Alexander Schueren <sha@amazon.com>
2023-12-21 16:21:01 +0100

    we can keep ParserOptions as type instead of interface

--- packages/parser/src/types/ParserOptions.ts
+++ packages/parser/src/types/ParserOptions.ts
@@ -1,9 +1,9 @@
 import type { ZodSchema } from 'zod';
 import { Envelope } from './envelope.js';
 
-interface ParserOptions<S extends ZodSchema> {
+type ParserOptions<S extends ZodSchema> = {
   schema: S;
   envelope?: Envelope;
-}
+};
 
 export { type ParserOptions };

a1a62c68c172f6c167f4a57240cb1d7b96929f77
Andrea Amorosi <dreamorosi@gmail.com>
2024-03-08 19:29:04 +0100

    chore: add method to avoid empty class

--- packages/jmespath/src/Functions.ts
+++ packages/jmespath/src/Functions.ts
@@ -1,2 +1,4 @@
 // This is a placeholder for the real class. The actual implementation will be added in a subsequent PR.
-export class Functions {}
+export class Functions {
+  public iAmAPlaceholder = true;
+}

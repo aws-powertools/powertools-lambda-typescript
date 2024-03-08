f0f2ad5b918c29874f052768f9e3f13e6de8d557
Alexander Schueren <sha@amazon.com>
2024-03-08 17:32:04 +0100

    use explicit exports instead of *

--- packages/parser/src/schemas/index.ts
+++ packages/parser/src/schemas/index.ts
@@ -1,17 +1,59 @@
-export * from './alb.js';
-export * from './apigw.js';
-export * from './apigwv2.js';
-export * from './cloudformation-custom-resource.js';
-export * from './cloudwatch.js';
-export * from './dynamodb.js';
-export * from './eventbridge.js';
-export * from './kafka.js';
-export * from './kinesis.js';
-export * from './kinesis-firehose.js';
-export * from './lambda.js';
-export * from './s3.js';
-export * from './ses.js';
-export * from './sns.js';
-export * from './sqs.js';
-export * from './vpc-lattice.js';
-export * from './vpc-latticev2.js';
+export { AlbSchema, AlbMultiValueHeadersSchema } from './alb.js';
+export { APIGatewayCert, APIGatewayProxyEventSchema } from './apigw.js';
+export { APIGatewayProxyEventV2Schema } from './apigwv2.js';
+export {
+  CloudFormationCustomResourceBaseSchema,
+  CloudFormationCustomResourceCreateSchema,
+  CloudFormationCustomResourceDeleteSchema,
+  CloudFormationCustomResourceUpdateSchema,
+} from './cloudformation-custom-resource.js';
+export {
+  CloudWatchLogEventSchema,
+  CloudWatchLogsDecodeSchema,
+  CloudWatchLogsSchema,
+  decompressRecordToJSON,
+} from './cloudwatch.js';
+export {
+  DynamoDBStreamChangeRecord,
+  DynamoDBStreamRecord,
+  DynamoDBStreamSchema,
+  UserIdentity,
+} from './dynamodb.js';
+export { EventBridgeSchema } from './eventbridge.js';
+export {
+  KafkaMskEventSchema,
+  KafkaRecordSchema,
+  KafkaSelfManagedEventSchema,
+} from './kafka.js';
+export {
+  KinesisDataStreamRecord,
+  KinesisDataStreamRecordPayload,
+  KinesisDataStreamSchema,
+} from './kinesis.js';
+export {
+  KinesisFirehoseSchema,
+  KinesisFirehoseSqsSchema,
+} from './kinesis-firehose.js';
+export { LambdaFunctionUrlSchema } from './lambda.js';
+export {
+  S3SqsEventNotificationSchema,
+  S3EventNotificationEventBridgeSchema,
+  S3ObjectLambdaEventSchema,
+  S3Schema,
+} from './s3.js';
+export { SesRecordSchema, SesSchema } from './ses.js';
+export {
+  SnsMsgAttribute,
+  SnsNotificationSchema,
+  SnsRecordSchema,
+  SnsSchema,
+  SnsSqsNotificationSchema,
+} from './sns.js';
+export {
+  SqsAttributesSchema,
+  SqsMsgAttributeSchema,
+  SqsRecordSchema,
+  SqsSchema,
+} from './sqs.js';
+export { VpcLatticeSchema } from './vpc-lattice.js';
+export { VpcLatticeV2Schema } from './vpc-latticev2.js';

1eb235a140146c98f1e56dccb715956a62be87ac
Andrea Amorosi <dreamorosi@gmail.com>
2024-04-16 11:59:56 +0200

    test(maintenance): add esm output to e2e tests

--- packages/testing/src/resources/TestNodejsFunction.ts
+++ packages/testing/src/resources/TestNodejsFunction.ts
@@ -1,6 +1,10 @@
 import { CfnOutput, Duration } from 'aws-cdk-lib';
 import { Tracing } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import {
+  NodejsFunction,
+  BundlingOptions,
+  OutputFormat,
+} from 'aws-cdk-lib/aws-lambda-nodejs';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { randomUUID } from 'node:crypto';
 import { TEST_RUNTIMES, TEST_ARCHITECTURES } from '../constants.js';
@@ -23,6 +27,17 @@ class TestNodejsFunction extends NodejsFunction {
     props: TestNodejsFunctionProps,
     extraProps: ExtraTestProps
   ) {
+    const isESM = extraProps.outputFormat === 'ESM' ? true : false;
+    const bundling: BundlingOptions = {
+      minify: true,
+      mainFields: isESM ? ['module', 'main'] : ['main', 'module'],
+      sourceMap: true,
+      format: isESM ? OutputFormat.ESM : OutputFormat.CJS,
+      banner: isESM
+        ? `import { createRequire } from 'module';const require = createRequire(import.meta.url);`
+        : '',
+    };
+
     super(stack.stack, `fn-${randomUUID().substring(0, 5)}`, {
       timeout: Duration.seconds(30),
       memorySize: 256,
@@ -35,6 +50,7 @@ class TestNodejsFunction extends NodejsFunction {
       runtime: TEST_RUNTIMES[getRuntimeKey()],
       architecture: TEST_ARCHITECTURES[getArchitectureKey()],
       logRetention: RetentionDays.ONE_DAY,
+      bundling,
     });
 
     new CfnOutput(this, extraProps.nameSuffix, {

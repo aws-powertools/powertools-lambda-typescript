7b41f6a7be432d609702947dcc4263e6ee1d16df
arnabrahman <arnabrahman@hotmail.com>
2024-05-22 14:22:45 +0600

    doc: minor description change for deepSort function

--- packages/idempotency/src/deepSort.ts
+++ packages/idempotency/src/deepSort.ts
@@ -18,12 +18,11 @@ const sortObject = (object: JSONObject): JSONObject => {
 /**
  * Recursively sorts the keys of an object or elements of an array.
  *
- * This function sorts the keys of any JSON object in a case-insensitive manner,
- * and recursively applies the same sorting to nested objects and arrays.
- * Primitives (strings, numbers, booleans, null) are returned unchanged.
+ * This function sorts the keys of any JSON in a case-insensitive manner and recursively applies the same sorting to
+ * nested objects and arrays. Primitives (strings, numbers, booleans, null) are returned unchanged.
  *
- * @param {JSONValue} data - The input data to be sorted, which can be an object, array, or primitive value.
- * @returns {JSONValue} - The sorted data, with all objects' keys sorted alphabetically in a case-insensitive manner.
+ * @param {JSONValue} data - The input data to be sorted, which can be an object, array or primitive value.
+ * @returns {JSONValue} - The sorted data, with all object's keys sorted alphabetically in a case-insensitive manner.
  */
 
 const deepSort = (data: JSONValue): JSONValue => {

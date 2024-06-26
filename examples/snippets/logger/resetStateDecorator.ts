e848283fe1d80ace4d32155504f41af688b29cae
Andrea Amorosi <dreamorosi@gmail.com>
2024-06-26 17:35:05 +0200

    docs(logger): rework attributes doc sections

---
+++ examples/snippets/logger/resetStateDecorator.ts
@@ -0,0 +1,30 @@
+import { Logger } from '@aws-lambda-powertools/logger';
+import type { LambdaInterface } from '@aws-lambda-powertools/commons/types';
+
+// Persistent attributes will be cached across invocations
+const logger = new Logger({
+  logLevel: 'info',
+  persistentLogAttributes: {
+    environment: 'prod',
+  },
+});
+
+class Lambda implements LambdaInterface {
+  @logger.injectLambdaContext({ resetState: true })
+  public async handler(
+    event: { userId: string },
+    _context: unknown
+  ): Promise<void> {
+    // This temporary key will be included in the log & cleared after the invocation
+    logger.appendKeys({
+      details: { userId: event.userId },
+    });
+
+    // ... your business logic
+
+    logger.info('WIDE');
+  }
+}
+
+const myFunction = new Lambda();
+export const handler = myFunction.handler.bind(myFunction); // (1)
---
+++ examples/snippets/logger/resetStateManual.ts
@@ -0,0 +1,27 @@
+import { Logger } from '@aws-lambda-powertools/logger';
+
+// Persistent attributes will be cached across invocations
+const logger = new Logger({
+  logLevel: 'info',
+  persistentLogAttributes: {
+    environment: 'prod',
+  },
+});
+
+// Enable the clear state flag
+export const handler = async (
+  event: { userId: string },
+  _context: unknown
+): Promise<void> => {
+  try {
+    // This temporary key will be included in the log & cleared after the invocation
+    logger.appendKeys({
+      details: { userId: event.userId },
+    });
+
+    // ... your business logic
+  } finally {
+    logger.info('WIDE');
+    logger.resetState();
+  }
+};

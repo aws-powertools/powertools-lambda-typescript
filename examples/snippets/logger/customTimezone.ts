0903e5a1635e39dd1a205a61a9f6eca37eb68482
arnabrahman <arnabrahman@hotmail.com>
2024-06-29 17:39:08 +0600

    doc: fix the special note

--- examples/snippets/logger/customTimezone.ts
+++ examples/snippets/logger/customTimezone.ts
@@ -4,7 +4,7 @@ export const handler = async (_event, _context): Promise<void> => {
   const loggerInUTC = new Logger({ serviceName: 'payment' });
   loggerInUTC.info('Logging with default AWS Lambda timezone: UTC time');
 
-  process.env.TZ = 'US/Eastern'; // (1)
+  process.env.TZ = 'US/Eastern'; // (1)!
 
   const logger = new Logger({ serviceName: 'order' });
   logger.info('Logging with US Eastern timezone');

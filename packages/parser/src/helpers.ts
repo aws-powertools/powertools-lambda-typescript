9ee798155490b0aa8fda5bf300b13ce405b86d4d
Alexander Schueren <sha@amazon.com>
2024-08-08 08:30:31 +0200

    add docstrings to helper functions

--- packages/parser/src/helpers.ts
+++ packages/parser/src/helpers.ts
@@ -1,5 +1,27 @@
 import { type ZodSchema, z } from 'zod';
 
+/**
+ * A helper function to parse a JSON string and validate it against a schema.
+ * Use it for built-in schemas like `AlbSchema`, `ApiGatewaySchema`, etc. to extend them with your customer schema.
+ *
+ * @example
+ * ```typescript
+ * import { JSONStringified } from '@aws-lambda-powertools/parser/helpers';
+ * import { AlbSchema } from '@aws-lambda-powertools/parser/schemas';
+ * import { z } from 'zod';
+ *
+ * const customSchema = z.object({
+ *   name: z.string(),
+ *   age: z.number(),
+ * });
+ *
+ * const extendedSchema = AlbSchema.extend({
+ *   body: JSONStringified(customSchema), // (1)!
+ * });
+ * ```
+ *
+ * @param schema - The schema to validate the JSON string against
+ */
 const JSONStringified = (schema: ZodSchema) =>
   z
     .string()

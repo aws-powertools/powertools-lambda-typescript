5eadaef3872f301d00d08346cbd959a46cd6430b
Andrea Amorosi <dreamorosi@gmail.com>
2024-08-16 12:16:18 +0200

    docs(logger): add logEventIfEnabled() docs (#2924)
    Co-authored-by: Leandro Damascena <leandro.damascena@gmail.com>

---
+++ examples/snippets/logger/logEventManual.ts
@@ -0,0 +1,10 @@
+process.env.POWERTOOLS_LOGGER_LOG_EVENT = 'true';
+
+import { Logger } from '@aws-lambda-powertools/logger';
+
+const logger = new Logger();
+
+export const handler = async (event: unknown) => {
+  logger.logEventIfEnabled(event); // (1)
+  // ... your logic here
+};

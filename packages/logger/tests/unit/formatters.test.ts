80a23336d6c3389848d71c4234dbafb0ca91a0e6
arnabrahman <arnabrahman@hotmail.com>
2024-09-02 11:07:45 +0600

    test: formatter when `logRecordOrder` is not set

--- packages/logger/tests/unit/formatters.test.ts
+++ packages/logger/tests/unit/formatters.test.ts
@@ -190,6 +190,175 @@ describe('Formatters', () => {
     });
   });
 
+  it('when logRecordOrder is set, it orders the attributes in the log item', () => {
+    // Prepare
+    const formatter = new PowertoolsLogFormatter({
+      logRecordOrder: ['message', 'timestamp', 'serviceName', 'environment'],
+    });
+    const unformattedAttributes: UnformattedAttributes = {
+      sampleRateValue: 0.25,
+      awsRegion: 'eu-west-1',
+      environment: 'prod',
+      serviceName: 'hello-world',
+      xRayTraceId: '1-5759e988-bd862e3fe1be46a994272793',
+      logLevel: 'WARN',
+      timestamp: new Date(),
+      message: 'This is a WARN log',
+      error: new Error('Something happened!'),
+      lambdaContext: {
+        functionName: 'my-lambda-function',
+        memoryLimitInMB: '123',
+        functionVersion: '1.23.3',
+        coldStart: true,
+        invokedFunctionArn:
+          'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        awsRequestId: 'abcdefg123456789',
+      },
+    };
+    const additionalLogAttributes: LogAttributes = {};
+
+    // Act
+    const value = formatter.formatAttributes(
+      unformattedAttributes,
+      additionalLogAttributes
+    );
+
+    const response = value.getAttributes();
+
+    // Assess
+    expect(JSON.stringify(response)).toEqual(
+      JSON.stringify({
+        message: 'This is a WARN log',
+        timestamp: '2016-06-20T12:08:10.000Z',
+        cold_start: true,
+        function_arn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        function_memory_size: '123',
+        function_name: 'my-lambda-function',
+        function_request_id: 'abcdefg123456789',
+        level: 'WARN',
+        sampling_rate: 0.25,
+        service: 'hello-world',
+        xray_trace_id: '1-5759e988-bd862e3fe1be46a994272793',
+      })
+    );
+  });
+
+  it('when logRecordOrder is set, it orders the attributes in the log item taking `additionalLogAttributes` into consideration', () => {
+    // Prepare
+    const formatter = new PowertoolsLogFormatter({
+      logRecordOrder: [
+        'message',
+        'additional_key',
+        'timestamp',
+        'serviceName',
+        'environment',
+      ],
+    });
+    const unformattedAttributes: UnformattedAttributes = {
+      sampleRateValue: 0.25,
+      awsRegion: 'eu-west-1',
+      environment: 'prod',
+      serviceName: 'hello-world',
+      xRayTraceId: '1-5759e988-bd862e3fe1be46a994272793',
+      logLevel: 'WARN',
+      timestamp: new Date(),
+      message: 'This is a WARN log',
+      error: new Error('Something happened!'),
+      lambdaContext: {
+        functionName: 'my-lambda-function',
+        memoryLimitInMB: '123',
+        functionVersion: '1.23.3',
+        coldStart: true,
+        invokedFunctionArn:
+          'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        awsRequestId: 'abcdefg123456789',
+      },
+    };
+    const additionalLogAttributes: LogAttributes = {
+      additional_key: 'additional_value',
+    };
+
+    // Act
+    const value = formatter.formatAttributes(
+      unformattedAttributes,
+      additionalLogAttributes
+    );
+
+    const response = value.getAttributes();
+
+    // Assess
+    expect(JSON.stringify(response)).toEqual(
+      JSON.stringify({
+        message: 'This is a WARN log',
+        additional_key: 'additional_value',
+        timestamp: '2016-06-20T12:08:10.000Z',
+        cold_start: true,
+        function_arn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        function_memory_size: '123',
+        function_name: 'my-lambda-function',
+        function_request_id: 'abcdefg123456789',
+        level: 'WARN',
+        sampling_rate: 0.25,
+        service: 'hello-world',
+        xray_trace_id: '1-5759e988-bd862e3fe1be46a994272793',
+      })
+    );
+  });
+
+  it('when logRecordOrder is not set, it will not order the attributes in the log item', () => {
+    // Prepare
+    const formatter = new PowertoolsLogFormatter({});
+    const unformattedAttributes: UnformattedAttributes = {
+      sampleRateValue: 0.25,
+      awsRegion: 'eu-west-1',
+      environment: 'prod',
+      serviceName: 'hello-world',
+      xRayTraceId: '1-5759e988-bd862e3fe1be46a994272793',
+      logLevel: 'WARN',
+      timestamp: new Date(),
+      message: 'This is a WARN log',
+      error: new Error('Something happened!'),
+      lambdaContext: {
+        functionName: 'my-lambda-function',
+        memoryLimitInMB: '123',
+        functionVersion: '1.23.3',
+        coldStart: true,
+        invokedFunctionArn:
+          'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        awsRequestId: 'abcdefg123456789',
+      },
+    };
+    const additionalLogAttributes: LogAttributes = {
+      additional_key: 'additional_value',
+    };
+
+    // Act
+    const value = formatter.formatAttributes(
+      unformattedAttributes,
+      additionalLogAttributes
+    );
+
+    const response = value.getAttributes();
+
+    // Assess
+    expect(JSON.stringify(response)).toEqual(
+      JSON.stringify({
+        cold_start: true,
+        function_arn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
+        function_memory_size: '123',
+        function_name: 'my-lambda-function',
+        function_request_id: 'abcdefg123456789',
+        level: 'WARN',
+        message: 'This is a WARN log',
+        sampling_rate: 0.25,
+        service: 'hello-world',
+        timestamp: '2016-06-20T12:08:10.000Z',
+        xray_trace_id: '1-5759e988-bd862e3fe1be46a994272793',
+        additional_key: 'additional_value',
+      })
+    );
+  });
+
   // #region format errors
 
   it.each([
@@ -656,120 +825,5 @@ describe('Formatters', () => {
       message: 'foo',
       timestamp: expect.any(String),
     });
-  });
-
-  it('when logRecordOrder is set, it orders the attributes in the log item', () => {
-    // Prepare
-    const formatter = new PowertoolsLogFormatter({
-      logRecordOrder: ['message', 'timestamp', 'serviceName', 'environment'],
-    });
-    const unformattedAttributes: UnformattedAttributes = {
-      sampleRateValue: 0.25,
-      awsRegion: 'eu-west-1',
-      environment: 'prod',
-      serviceName: 'hello-world',
-      xRayTraceId: '1-5759e988-bd862e3fe1be46a994272793',
-      logLevel: 'WARN',
-      timestamp: new Date(),
-      message: 'This is a WARN log',
-      error: new Error('Something happened!'),
-      lambdaContext: {
-        functionName: 'my-lambda-function',
-        memoryLimitInMB: '123',
-        functionVersion: '1.23.3',
-        coldStart: true,
-        invokedFunctionArn:
-          'arn:aws:lambda:eu-west-1:123456789012:function:Example',
-        awsRequestId: 'abcdefg123456789',
-      },
-    };
-    const additionalLogAttributes: LogAttributes = {};
-
-    // Act
-    const value = formatter.formatAttributes(
-      unformattedAttributes,
-      additionalLogAttributes
-    );
-
-    const response = value.getAttributes();
-
-    // Assess
-    expect(JSON.stringify(response)).toEqual(
-      JSON.stringify({
-        message: 'This is a WARN log',
-        timestamp: '2016-06-20T12:08:10.000Z',
-        cold_start: true,
-        function_arn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
-        function_memory_size: '123',
-        function_name: 'my-lambda-function',
-        function_request_id: 'abcdefg123456789',
-        level: 'WARN',
-        sampling_rate: 0.25,
-        service: 'hello-world',
-        xray_trace_id: '1-5759e988-bd862e3fe1be46a994272793',
-      })
-    );
-  });
-
-  it('when logRecordOrder is set, it orders the attributes in the log item taking `additionalLogAttributes` into consideration', () => {
-    // Prepare
-    const formatter = new PowertoolsLogFormatter({
-      logRecordOrder: [
-        'message',
-        'additional_key',
-        'timestamp',
-        'serviceName',
-        'environment',
-      ],
-    });
-    const unformattedAttributes: UnformattedAttributes = {
-      sampleRateValue: 0.25,
-      awsRegion: 'eu-west-1',
-      environment: 'prod',
-      serviceName: 'hello-world',
-      xRayTraceId: '1-5759e988-bd862e3fe1be46a994272793',
-      logLevel: 'WARN',
-      timestamp: new Date(),
-      message: 'This is a WARN log',
-      error: new Error('Something happened!'),
-      lambdaContext: {
-        functionName: 'my-lambda-function',
-        memoryLimitInMB: '123',
-        functionVersion: '1.23.3',
-        coldStart: true,
-        invokedFunctionArn:
-          'arn:aws:lambda:eu-west-1:123456789012:function:Example',
-        awsRequestId: 'abcdefg123456789',
-      },
-    };
-    const additionalLogAttributes: LogAttributes = {
-      additional_key: 'additional_value',
-    };
-
-    // Act
-    const value = formatter.formatAttributes(
-      unformattedAttributes,
-      additionalLogAttributes
-    );
-
-    const response = value.getAttributes();
-
-    // Assess
-    expect(JSON.stringify(response)).toEqual(
-      JSON.stringify({
-        message: 'This is a WARN log',
-        additional_key: 'additional_value',
-        timestamp: '2016-06-20T12:08:10.000Z',
-        cold_start: true,
-        function_arn: 'arn:aws:lambda:eu-west-1:123456789012:function:Example',
-        function_memory_size: '123',
-        function_name: 'my-lambda-function',
-        function_request_id: 'abcdefg123456789',
-        level: 'WARN',
-        sampling_rate: 0.25,
-        service: 'hello-world',
-        xray_trace_id: '1-5759e988-bd862e3fe1be46a994272793',
-      })
-    );
   });
 });

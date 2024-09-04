ff6fb6e46a9d9f222d0ac9a94946e015eb6f40b3
Andrea Amorosi <dreamorosi@gmail.com>
2024-09-04 16:07:48 +0100

    Update packages/tracer/tests/helpers/invokeAllTests.ts
    Co-authored-by: Alexander Schueren <sha@amazon.com>

--- packages/tracer/tests/helpers/invokeAllTests.ts
+++ packages/tracer/tests/helpers/invokeAllTests.ts
@@ -4,7 +4,6 @@ import { invokeFunction } from '@aws-lambda-powertools/testing-utils';
  * Invoke function sequentially 3 times with different parameters
  *
  * invocation: is just a tracking number (it has to start from 1)
- * sdkV2: define if we will use `captureAWSClient()` or `captureAWS()` for SDK V2
  * throw: forces the Lambda to throw an error
  *
  * @param functionName

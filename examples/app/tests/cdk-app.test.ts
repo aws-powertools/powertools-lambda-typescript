ad323aeedd88c56fd5a5d2c3e718bb97a8e3f55a
Andrea Amorosi <dreamorosi@gmail.com>
2024-09-06 13:25:23 +0200

    docs(maintenance): fix install instructions & switch test runner (#3030)

--- examples/app/tests/cdk-app.test.ts
+++ examples/app/tests/cdk-app.test.ts
@@ -1,9 +1,13 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { describe, it } from 'vitest';
 import { PowertoolsExampleStack } from '../cdk/example-stack.js';
 
-test('CDK code synthesize', () => {
+describe('CDK example stack', () => {
   const app = new App();
   const stack = new PowertoolsExampleStack(app, 'MyTestStack');
-  Template.fromStack(stack).resourceCountIs('AWS::Lambda::Function', 4);
+
+  it('has 4 Lambda functions', () => {
+    Template.fromStack(stack).resourceCountIs('AWS::Lambda::Function', 4);
+  });
 });

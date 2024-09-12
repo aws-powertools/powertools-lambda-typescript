3502f8a514d878ec56d5950812797e8ca6d13977
Andrea Amorosi <dreamorosi@gmail.com>
2024-09-12 23:35:00 +0200

    improv(tracer): move env variable out of barrel file

--- packages/tracer/src/Tracer.ts
+++ packages/tracer/src/Tracer.ts
@@ -1,3 +1,15 @@
+/**
+ * If the AWS_XRAY_CONTEXT_MISSING environment variable is not set, we set it to IGNORE_ERROR.
+ *
+ * This is to prevent the AWS X-Ray SDK from logging errors when using top-level await features that make HTTP requests.
+ * For example, when using the Parameters utility to fetch parameters during the initialization of the Lambda handler - See #2046
+ */
+if (
+  process.env.AWS_XRAY_CONTEXT_MISSING === '' ||
+  process.env.AWS_XRAY_CONTEXT_MISSING === undefined
+) {
+  process.env.AWS_XRAY_CONTEXT_MISSING = 'IGNORE_ERROR';
+}
 import { Utility } from '@aws-lambda-powertools/commons';
 import type {
   AsyncHandler,

10a339b757dfa2364d622be0c96638ad33fd587e
arnabrahman <arnabrahman@hotmail.com>
2024-09-15 16:14:01 +0600

    doc: update `determineResultFromIdempotencyRecord` doc

--- packages/idempotency/src/IdempotencyHandler.ts
+++ packages/idempotency/src/IdempotencyHandler.ts
@@ -87,6 +87,8 @@ export class IdempotencyHandler<Func extends AnyFunction> {
   /**
    * Takes an idempotency key and returns the idempotency record from the persistence layer.
    *
+   * If a response hook is provided in the idempotency configuration, it will be called to before returning the response.
+   *
    * If the idempotency record is not COMPLETE, then it will throw an error based on the status of the record.
    *
    * @param idempotencyRecord The idempotency record stored in the persistence layer

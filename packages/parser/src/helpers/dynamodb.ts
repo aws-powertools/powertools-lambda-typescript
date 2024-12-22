a1f9b8cca3dbf76e3a9c3af0f704eec87566a496
arnabrahman <arnabrahman@hotmail.com>
2024-12-22 17:52:09 +0600

    doc: `DynamoDBMarshalled` function doc description

--- packages/parser/src/helpers/dynamodb.ts
+++ packages/parser/src/helpers/dynamodb.ts
@@ -2,6 +2,64 @@ import type { AttributeValue } from '@aws-sdk/client-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { type ZodTypeAny, z } from 'zod';
 
+/**
+ * A helper function to unmarshall DynamoDB stream events and validate them against a schema.
+ *
+ * @example
+ * ```typescript
+ * const mySchema = z.object({
+ *   id: z.string(),
+ *   name: z.string(),
+ * });
+ * const eventSchema = DynamoDBStreamSchema.extend({
+ *   Records: z.array(
+ *     DynamoDBStreamRecord.extend({
+ *       dynamodb: z.object({
+ *         NewImage: DynamoDBMarshalled(mySchema).optional(),
+ *       }),
+ *     })
+ *   ),
+ * });
+ * ```
+ * For example, if you have a DynamoDB stream event like the following:
+ *
+ * ```json
+ * {
+ *   "Records": [
+ *     {
+ *       "dynamodb": {
+ *         "NewImage": {
+ *           "id": {
+ *             "S": "12345"
+ *           },
+ *           "name": {
+ *             "S": "John Doe"
+ *           }
+ *         }
+ *       }
+ *     }
+ *   ]
+ * }
+ * ```
+ * Resulting in:
+ *
+ * ```json
+ * {
+ *   "Records": [
+ *     {
+ *       "dynamodb": {
+ *         "NewImage": {
+ *           "id": "12345",
+ *           "name": "John Doe"
+ *         }
+ *       }
+ *     }
+ *   ]
+ * }
+ * ```
+ *
+ * @param schema - The schema to validate the JSON string against
+ */
 const DynamoDBMarshalled = <T extends ZodTypeAny>(schema: T) =>
   z
     .record(z.string(), z.custom<AttributeValue>())

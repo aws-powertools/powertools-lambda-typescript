02940140f24582f3a26fa4dbf5b328f1d3e03667
arnabrahman <arnabrahman@hotmail.com>
2024-12-22 11:04:35 +0600

    feat: `DynamoDBMarshalled` function

--- packages/parser/src/helpers.ts
+++ packages/parser/src/helpers.ts
@@ -1,3 +1,5 @@
+import type { AttributeValue } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 import { type ZodTypeAny, z } from 'zod';
 /**
  * @typedef {import('../schemas/alb').AlbSchema} AlbSchema
@@ -56,4 +58,22 @@ const JSONStringified = <T extends ZodTypeAny>(schema: T) =>
     })
     .pipe(schema);
 
-export { JSONStringified };
+const DynamoDBMarshalled = <T extends ZodTypeAny>(schema: T) =>
+  z
+    .record(z.string(), z.custom<AttributeValue>())
+    .transform((str, ctx) => {
+      try {
+        return unmarshall(str);
+      } catch (err) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Could not unmarshall DynamoDB stream record',
+          fatal: true,
+        });
+
+        return z.NEVER;
+      }
+    })
+    .pipe(schema);
+
+export { JSONStringified, DynamoDBMarshalled };

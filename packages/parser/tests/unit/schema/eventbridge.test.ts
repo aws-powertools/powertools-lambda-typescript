7cce60b41b8b72c5aeee5eb17b87d159af3123ac
Andrea Amorosi <dreamorosi@gmail.com>
2025-01-21 12:00:58 +0100

    fix(parser): EventBridge envelope uses correct path (#3504)

--- packages/parser/tests/unit/schema/eventbridge.test.ts
+++ packages/parser/tests/unit/schema/eventbridge.test.ts
@@ -1,11 +1,30 @@
 import { describe, expect, it } from 'vitest';
-import { EventBridgeSchema } from '../../../src/schemas/';
-import { TestEvents } from './utils.js';
+import { EventBridgeSchema } from '../../../src/schemas/eventbridge.js';
+import type { EventBridgeEvent } from '../../../src/types/schema.js';
+import { getTestEvent, omit } from '../schema/utils.js';
 
-describe('EventBridge ', () => {
-  it('should parse eventbridge event', () => {
-    const eventBridgeEvent = TestEvents.eventBridgeEvent;
+describe('Schema: EventBridge', () => {
+  const baseEvent = getTestEvent<EventBridgeEvent>({
+    eventsPath: 'eventbridge',
+    filename: 'base',
+  });
 
-    expect(EventBridgeSchema.parse(eventBridgeEvent)).toEqual(eventBridgeEvent);
+  it('parses an EventBridge event', () => {
+    // Prepare
+    const event = structuredClone(baseEvent);
+
+    // Act
+    const result = EventBridgeSchema.parse(event);
+
+    // Assess
+    expect(result).toStrictEqual(event);
+  });
+
+  it('throws if the event is not an EventBridge event', () => {
+    // Prepare
+    const event = omit(['detail', 'detail-type'], structuredClone(baseEvent));
+
+    // Act & Assess
+    expect(() => EventBridgeSchema.parse(event)).toThrow();
   });
 });

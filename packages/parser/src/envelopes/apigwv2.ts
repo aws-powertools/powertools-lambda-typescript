a4846afd09c29032f79b79cfa1410675440d7dae
Andrea Amorosi <dreamorosi@gmail.com>
2025-01-23 09:54:41 +0100

    fix(parser): API Gateway Envelopes handle non-JSON (#3511)

--- packages/parser/src/envelopes/apigwv2.ts
+++ packages/parser/src/envelopes/apigwv2.ts
@@ -2,7 +2,7 @@ import type { ZodSchema, z } from 'zod';
 import { ParseError } from '../errors.js';
 import { APIGatewayProxyEventV2Schema } from '../schemas/apigwv2.js';
 import type { ParsedResult } from '../types/index.js';
-import { Envelope, envelopeDiscriminator } from './envelope.js';
+import { envelopeDiscriminator } from './envelope.js';
 
 /**
  * API Gateway V2 envelope to extract data within body key
@@ -14,40 +14,38 @@ export const ApiGatewayV2Envelope = {
    */
   [envelopeDiscriminator]: 'object' as const,
   parse<T extends ZodSchema>(data: unknown, schema: T): z.infer<T> {
-    return Envelope.parse(
-      APIGatewayProxyEventV2Schema.parse(data).body,
-      schema
-    );
+    try {
+      return APIGatewayProxyEventV2Schema.extend({
+        body: schema,
+      }).parse(data).body;
+    } catch (error) {
+      throw new ParseError('Failed to parse API Gateway HTTP body', {
+        cause: error as Error,
+      });
+    }
   },
 
   safeParse<T extends ZodSchema>(
     data: unknown,
     schema: T
   ): ParsedResult<unknown, z.infer<T>> {
-    const parsedEnvelope = APIGatewayProxyEventV2Schema.safeParse(data);
-    if (!parsedEnvelope.success) {
+    const result = APIGatewayProxyEventV2Schema.extend({
+      body: schema,
+    }).safeParse(data);
+
+    if (!result.success) {
       return {
         success: false,
-        error: new ParseError('Failed to parse API Gateway V2 envelope', {
-          cause: parsedEnvelope.error,
+        error: new ParseError('Failed to parse API Gateway HTTP body', {
+          cause: result.error,
         }),
         originalEvent: data,
       };
     }
 
-    const parsedBody = Envelope.safeParse(parsedEnvelope.data.body, schema);
-
-    if (!parsedBody.success) {
-      return {
-        success: false,
-        error: new ParseError('Failed to parse API Gateway V2 envelope body', {
-          cause: parsedBody.error,
-        }),
-        originalEvent: data,
-      };
-    }
-
-    // use type assertion to avoid type check, we know it's success here
-    return parsedBody;
+    return {
+      success: true,
+      data: result.data.body,
+    };
   },
 };

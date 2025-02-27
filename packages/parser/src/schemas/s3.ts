ae38d6219f801f1bdf92739fecc7813bf6fc3241
Connor Kirkpatrick <ckp@amazon.com>
2025-02-27 11:25:08 +0000

    Make sequencer field optional
    The sequencer field is only present in PUT and DELETE events.

--- packages/parser/src/schemas/s3.ts
+++ packages/parser/src/schemas/s3.ts
@@ -24,7 +24,7 @@ const S3Message = z.object({
     size: z.number().optional(),
     urlDecodedKey: z.string().optional(),
     eTag: z.string().optional(),
-    sequencer: z.string(),
+    sequencer: z.string().optional(), // Only present in PUT and DELETE events
     versionId: z.optional(z.string()),
   }),
   bucket: z.object({

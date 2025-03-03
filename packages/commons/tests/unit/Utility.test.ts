2ca202706481b0750aada9982f4646f7c7828787
Andrea Amorosi <dreamorosi@gmail.com>
2025-03-03 12:10:53 +0100

    chore(maintenance): simplify Utility class (#3682)

--- packages/commons/tests/unit/Utility.test.ts
+++ packages/commons/tests/unit/Utility.test.ts
@@ -1,157 +1,57 @@
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Utility } from '../../src/index.js';
 
 describe('Class: Utility', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-    vi.resetModules();
-  });
-
-  describe('Method: getDefaultServiceName', () => {
-    it('returns the default service name', () => {
-      class PowerTool extends Utility {
-        public dummyMethod(): string {
-          return this.getDefaultServiceName();
-        }
-      }
-
-      const powertool = new PowerTool();
-      const result = powertool.dummyMethod();
-      expect(result).toBe('service_undefined');
-    });
-  });
-
-  describe('Method: getColdStart', () => {
-    it('it returns true the first time, then false afterwards, when called multiple times', () => {
-      // Prepare
-      const utility = new Utility();
-      const getColdStartSpy = vi.spyOn(utility, 'getColdStart');
-
-      // Act
-      utility.getColdStart();
-      utility.getColdStart();
-      utility.getColdStart();
-      utility.getColdStart();
-      utility.getColdStart();
-
-      // Assess
-      expect(getColdStartSpy).toHaveBeenCalledTimes(5);
-      expect(getColdStartSpy.mock.results).toEqual([
-        expect.objectContaining({ value: true }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-      ]);
-    });
-
-    it('returns the correct values when subclassed', () => {
-      // Prepare
-      class PowerTool extends Utility {
-        public dummyMethod(): boolean {
-          return this.getColdStart();
-        }
-      }
-      const powertool = new PowerTool();
-      const dummyMethodSpy = vi.spyOn(powertool, 'dummyMethod');
-      const getColdStartSpy = vi.spyOn(powertool, 'getColdStart');
-
-      // Act
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-
-      // Assess
-      expect(dummyMethodSpy).toHaveBeenCalledTimes(5);
-      expect(getColdStartSpy).toHaveBeenCalledTimes(5);
-      expect(dummyMethodSpy.mock.results).toEqual([
-        expect.objectContaining({ value: true }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-      ]);
-    });
-  });
-
-  describe('Method: isColdStart', () => {
-    it('returns true the first time, then false afterwards when called multiple times', () => {
-      // Prepare
-      const utility = new Utility();
-      const isColdStartSpy = vi.spyOn(utility, 'isColdStart');
-
-      // Act
-      utility.isColdStart();
-      utility.isColdStart();
-      utility.isColdStart();
-      utility.isColdStart();
-      utility.isColdStart();
-
-      // Assess
-      expect(isColdStartSpy).toHaveBeenCalledTimes(5);
-      expect(isColdStartSpy.mock.results).toEqual([
-        expect.objectContaining({ value: true }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-      ]);
-    });
-
-    it('returns the correct values when subclassed', () => {
-      // Prepare
-      class PowerTool extends Utility {
-        public dummyMethod(): boolean {
-          return this.isColdStart();
-        }
-      }
-      const powertool = new PowerTool();
-      const dummyMethodSpy = vi.spyOn(powertool, 'dummyMethod');
-      const isColdStartSpy = vi.spyOn(powertool, 'isColdStart');
-
-      // Act
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-      powertool.dummyMethod();
-
-      // Assess
-      expect(dummyMethodSpy).toHaveBeenCalledTimes(5);
-      expect(isColdStartSpy).toHaveBeenCalledTimes(5);
-      expect(dummyMethodSpy.mock.results).toEqual([
-        expect.objectContaining({ value: true }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-        expect.objectContaining({ value: false }),
-      ]);
-    });
-  });
-
-  describe('Method: isValidServiceName', () => {
-    class PowerTool extends Utility {
-      public dummyMethod(name: string): boolean {
-        return this.isValidServiceName(name);
-      }
+  class TestUtility extends Utility {
+    public dummyMethod(): boolean {
+      return this.getColdStart();
     }
-    it('allows valid strings', () => {
-      const powertool = new PowerTool();
-      const goodName = 'serverlessAirline';
+    public isColdStart(): boolean {
+      return this.coldStart;
+    }
+    public getServiceName(): string {
+      return this.defaultServiceName;
+    }
+    public validateServiceName(serviceName: string): boolean {
+      return this.isValidServiceName(serviceName);
+    }
+  }
 
-      const result = powertool.dummyMethod(goodName);
+  it('returns the correct cold start value', () => {
+    // Prepare
+    const utility = new TestUtility();
 
-      expect(result).toBe(true);
-    });
+    // Act & Assess
+    expect(utility.dummyMethod()).toBe(true);
+    expect(utility.dummyMethod()).toBe(false);
+    expect(utility.dummyMethod()).toBe(false);
+  });
 
-    it("doesn't allow empty strings", () => {
-      const tooShort = '';
-      const powertool = new PowerTool();
-      const result = powertool.dummyMethod(tooShort);
+  it('flips the cold start value', () => {
+    // Prepare
+    const utility = new TestUtility();
 
-      expect(result).toBe(false);
-    });
+    // Act
+    utility.dummyMethod();
+
+    // Assess
+    expect(utility.isColdStart()).toBe(false);
+  });
+
+  it('returns the correct default service name', () => {
+    // Prepare
+    const utility = new TestUtility();
+
+    // Assess
+    expect(utility.getServiceName()).toBe('service_undefined');
+  });
+
+  it('validates service name', () => {
+    // Prepare
+    const utility = new TestUtility();
+
+    // Act & Assess
+    expect(utility.validateServiceName('serverlessAirline')).toBe(true);
+    expect(utility.validateServiceName('')).toBe(false);
   });
 });

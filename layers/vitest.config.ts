19eee8b75f53c2943fa38dcef27d925df4d5198e
Andrea Amorosi <dreamorosi@gmail.com>
2025-03-18 11:35:23 +0100

    chore: update config

--- layers/vitest.config.ts
+++ layers/vitest.config.ts
@@ -3,5 +3,7 @@ import { defineProject } from 'vitest/config';
 export default defineProject({
   test: {
     environment: 'node',
+    hookTimeout: 1_000 * 60 * 10, // 10 minutes
+    testTimeout: 1_000 * 60 * 3, // 3 minutes
   },
 });

63ca402019ec12e163d6a1aa8676377559a56aa9
Andrea Amorosi <dreamorosi@gmail.com>
2025-04-25 17:12:42 -0700

    chore: update snippets

--- examples/snippets/event-handler/appsync-events/accessEventAndContext.ts
+++ examples/snippets/event-handler/appsync-events/accessEventAndContext.ts
@@ -1,11 +1,16 @@
 import { AppSyncEventsResolver } from '@aws-lambda-powertools/event-handler/appsync-events';
+import { Logger } from '@aws-lambda-powertools/logger';
 import type { Context } from 'aws-lambda';
 
+const logger = new Logger({
+  serviceName: 'serverlessAirline',
+});
 const app = new AppSyncEventsResolver();
 
 app.onPublish('/*', (payload, event, context) => {
   const { headers } = event.request; // (1)!
   const { awsRequestId } = context;
+  logger.info('headers', { headers, awsRequestId });
 
   // your business logic here
 
@@ -13,4 +18,4 @@ app.onPublish('/*', (payload, event, context) => {
 });
 
 export const handler = async (event: unknown, context: Context) =>
-  await app.resolve(event, context);
+  app.resolve(event, context);

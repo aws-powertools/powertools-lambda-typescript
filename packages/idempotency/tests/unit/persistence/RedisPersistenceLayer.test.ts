479f3830422ecd7028fdff11d50ddb5e0d53bff2
arnabrahman <arnabrahman@hotmail.com>
2025-05-04 10:35:45 +0600

    test: ` _putRecord` unit tests

--- packages/idempotency/tests/unit/persistence/RedisPersistenceLayer.test.ts
+++ packages/idempotency/tests/unit/persistence/RedisPersistenceLayer.test.ts
@@ -7,19 +7,27 @@ import {
   it,
   vi,
 } from 'vitest';
+import { IdempotencyRecordStatus } from '../../../src/constants.js';
 import { IdempotencyPersistenceConnectionError } from '../../../src/errors.js';
+import { IdempotencyRecord } from '../../../src/persistence/IdempotencyRecord.js';
 import RedisConnection from '../../../src/persistence/RedisConnection.js';
-import { RedisPersistenceLayer } from '../../../src/persistence/RedisPersistenceLayer.js';
 import type { RedisClientProtocol } from '../../../src/types/RedisPersistence.js';
+import { RedisPersistenceLayerTestClass } from '../../helpers/idempotencyUtils.js';
 
 vi.mock('../../../src/persistence/RedisConnection.js');
 
-describe('Class: RedisPersistenceLayer', () => {
-  const mockDefaultClientConnect = vi.fn().mockResolvedValue(undefined);
+const getFutureTimestamp = (seconds: number): number =>
+  Math.floor(Date.now() / 1000) + seconds;
+
+describe('Class: RedisPersistenceLayerTestClass', () => {
+  const mockDefaultClientConnect = vi.fn().mockResolvedValue(true);
 
   const mockDefaultClient = {
     isOpen: false,
     connect: mockDefaultClientConnect,
+    set: vi.fn(),
+    get: vi.fn(),
+    del: vi.fn().mockResolvedValue(1),
   };
 
   const mockUserProvidedClient: RedisClientProtocol = {
@@ -43,7 +51,7 @@ describe('Class: RedisPersistenceLayer', () => {
   describe('Method: init', () => {
     it('should create a Redis connection and connect when no client is provided', async () => {
       // Prepare
-      const layer = new RedisPersistenceLayer({});
+      const layer = new RedisPersistenceLayerTestClass({});
 
       // Act
       await layer.init();
@@ -58,7 +66,7 @@ describe('Class: RedisPersistenceLayer', () => {
       mockDefaultClientConnect.mockRejectedValueOnce(
         new Error('Connection failed')
       );
-      const layer = new RedisPersistenceLayer({});
+      const layer = new RedisPersistenceLayerTestClass({});
       const consoleErrorSpy = vi
         .spyOn(console, 'error')
         .mockImplementation(() => {});
@@ -91,7 +99,7 @@ describe('Class: RedisPersistenceLayer', () => {
         getClient: vi.fn().mockReturnValue(connectedClient),
       }));
 
-      const layer = new RedisPersistenceLayer({});
+      const layer = new RedisPersistenceLayerTestClass({});
 
       // Act
       await layer.init();
@@ -103,7 +111,7 @@ describe('Class: RedisPersistenceLayer', () => {
 
     it('will do nothing if a user-provided client is used', async () => {
       // Prepare
-      const layer = new RedisPersistenceLayer({
+      const layer = new RedisPersistenceLayerTestClass({
         client: mockUserProvidedClient,
       });
 
@@ -113,6 +121,82 @@ describe('Class: RedisPersistenceLayer', () => {
       // Assess
       expect(RedisConnection).not.toHaveBeenCalled();
       expect(mockDefaultClientConnect).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('Method: _putRecord', () => {
+    const dummyKey = 'someKey';
+
+    describe('when a user-provided client is used', () => {
+      it('puts a record with INPROGRESS status into Redis', async () => {
+        // Prepare
+        const layer = new RedisPersistenceLayerTestClass({});
+        await layer.init();
+        const record = new IdempotencyRecord({
+          idempotencyKey: dummyKey,
+          status: IdempotencyRecordStatus.INPROGRESS,
+          expiryTimestamp: getFutureTimestamp(10),
+        });
+        mockDefaultClient.set.mockResolvedValue('OK');
+
+        // Act
+        await layer._putRecord(record);
+
+        // Assess
+        expect(mockDefaultClient.set).toHaveBeenCalledWith(
+          dummyKey,
+          JSON.stringify({
+            status: IdempotencyRecordStatus.INPROGRESS,
+            expiration: record.expiryTimestamp,
+          }),
+          expect.objectContaining({ EX: expect.any(Number), NX: true })
+        );
+      });
+
+      it('puts the record in Redis when using an in progress expiry timestamp', async () => {
+        // Prepare
+        const layer = new RedisPersistenceLayerTestClass({});
+        await layer.init();
+        const status = IdempotencyRecordStatus.INPROGRESS;
+        const expiryTimestamp = getFutureTimestamp(10);
+        const inProgressExpiryTimestamp = getFutureTimestamp(5);
+        const record = new IdempotencyRecord({
+          idempotencyKey: dummyKey,
+          status,
+          expiryTimestamp,
+          inProgressExpiryTimestamp,
+        });
+
+        // Act
+        await layer._putRecord(record);
+
+        // Assess
+        expect(mockDefaultClient.set).toHaveBeenCalledWith(
+          dummyKey,
+          JSON.stringify({
+            status,
+            expiration: expiryTimestamp,
+            in_progress_expiration: inProgressExpiryTimestamp,
+          }),
+          { EX: 10, NX: true }
+        );
+      });
+
+      it('throws error when trying to put a non-INPROGRESS record', async () => {
+        // Prepare
+        const layer = new RedisPersistenceLayerTestClass({});
+        await layer.init();
+        const record = new IdempotencyRecord({
+          idempotencyKey: dummyKey,
+          status: IdempotencyRecordStatus.COMPLETED,
+          expiryTimestamp: getFutureTimestamp(10),
+        });
+
+        // Act & Assess
+        await expect(layer._putRecord(record)).rejects.toThrow(
+          'Only INPROGRESS records can be inserted with _putRecord'
+        );
+      });
     });
   });
 });

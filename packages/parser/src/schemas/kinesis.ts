e46324b8e7a51070d68bedd87f43138e75ce0f2b
kiitosu <kh0412@gmail.com>
2025-05-15 17:51:00 +0900

    update comment

--- packages/parser/src/schemas/kinesis.ts
+++ packages/parser/src/schemas/kinesis.ts
@@ -70,37 +70,33 @@ const KinesisDynamoDBStreamSchema = z.object({
  *         "partitionKey": "1",
  *         "sequenceNumber": "49590338271490256608559692538361571095921575989136588898",
  *         "data": "SGVsbG8sIHRoaXMgaXMgYSB0ZXN0Lg==",
- *         "approximateArrivalTimestamp": 1545084650.987
+ *         "approximateArrivalTimestamp": 1607497475.000
  *       },
  *       "eventSource": "aws:kinesis",
  *       "eventVersion": "1.0",
  *       "eventID": "shardId-000000000006:49590338271490256608559692538361571095921575989136588898",
  *       "eventName": "aws:kinesis:record",
- *       "invokeIdentityArn": "arn:aws:iam::123456789012:role/lambda-role",
- *       "awsRegion": "us-east-2",
- *       "eventSourceARN": "arn:aws:kinesis:us-east-2:123456789012:stream/lambda-stream"
- *     },
- *     {
- *       "kinesis": {
- *         "kinesisSchemaVersion": "1.0",
- *         "partitionKey": "1",
- *         "sequenceNumber": "49590338271490256608559692540925702759324208523137515618",
- *         "data": "VGhpcyBpcyBvbmx5IGEgdGVzdC4=",
- *         "approximateArrivalTimestamp": 1545084711.166
- *       },
- *       "eventSource": "aws:kinesis",
- *       "eventVersion": "1.0",
- *       "eventID": "shardId-000000000006:49590338271490256608559692540925702759324208523137515618",
- *       "eventName": "aws:kinesis:record",
- *       "invokeIdentityArn": "arn:aws:iam::123456789012:role/lambda-role",
- *       "awsRegion": "us-east-2",
- *       "eventSourceARN": "arn:aws:kinesis:us-east-2:123456789012:stream/lambda-stream"
+ *       "invokeIdentityArn": "arn:aws:iam::123456789012:role/lambda-kinesis-role",
+ *       "awsRegion": "us-east-1",
+ *       "eventSourceARN": "arn:aws:kinesis:us-east-1:123456789012:stream/lambda-stream"
  *     }
- *   ]
+ *   ],
+ *   "window": {
+ *     "start": "2020-12-09T07:04:00Z",
+ *     "end": "2020-12-09T07:06:00Z"
+ *   },
+ *   "state": {
+ *     "1": 282,
+ *     "2": 715
+ *   },
+ *   "shardId": "shardId-000000000006",
+ *   "eventSourceARN": "arn:aws:kinesis:us-east-1:123456789012:stream/lambda-stream",
+ *   "isFinalInvokeForWindow": false,
+ *   "isWindowTerminatedEarly": false
  * }
  *```
  * @see {@link types.KinesisDataStreamEvent | KinesisDataStreamEvent}
- * @see {@link https://docs.aws.amazon.com/lambda/latest/dg/with-kinesis.html#services-kinesis-event-example}
+ * @see {@link https://docs.aws.amazon.com/lambda/latest/dg/services-kinesis-windows.html#streams-tumbling-processing}
  *
  */
 const KinesisDataStreamSchema = z.object({

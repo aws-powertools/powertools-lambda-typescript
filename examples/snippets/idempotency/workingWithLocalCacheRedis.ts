feead52075b42bbe77457741fe4eb6c494209145
arnabrahman <arnabrahman@hotmail.com>
2025-05-16 16:29:17 +0600

    refactor: remove `CacheClient` type

--- examples/snippets/idempotency/workingWithLocalCacheRedis.ts
+++ examples/snippets/idempotency/workingWithLocalCacheRedis.ts
@@ -1,14 +1,13 @@
 import { makeIdempotent } from '@aws-lambda-powertools/idempotency';
 import { CachePersistenceLayer } from '@aws-lambda-powertools/idempotency/cache';
-import type { CacheClient } from '@aws-lambda-powertools/idempotency/cache/types';
 import { createClient } from '@redis/client';
 import type { Context } from 'aws-lambda';
 
 // Initialize the Redis client
-const client = (await createClient({
+const client = await createClient({
   url: `rediss://${process.env.CACHE_ENDPOINT}:${process.env.CACHE_PORT}`,
   username: 'default',
-}).connect()) as CacheClient;
+}).connect();
 
 const cachePersistenceStore = new CachePersistenceLayer({
   client,

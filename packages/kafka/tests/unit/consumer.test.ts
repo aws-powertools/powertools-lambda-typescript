b3ec3452c5b52534b0cb3b9f3f3d4e51ff1150bb
Alexander Schueren <sha@amazon.com>
2025-06-04 18:21:47 +0200

    fix tests

--- packages/kafka/tests/unit/consumer.test.ts
+++ packages/kafka/tests/unit/consumer.test.ts
@@ -6,6 +6,7 @@ import { kafkaConsumer } from '../../src/consumer.js';
 import type { ConsumerRecords } from '../../src/types.js';
 import * as avroEvent from '../events/avro.json' with { type: 'json' };
 import * as jsonEvent from '../events/default.json' with { type: 'json' };
+import * as protobufEvent from '../events/protobuf.json' with { type: 'json' };
 
 describe('Kafka consumer: ', () => {
   //{   "id": 12345,   "name": "product5",   "price": 45 }
@@ -80,6 +81,34 @@ describe('Kafka consumer: ', () => {
       value: { id: 1001, name: 'Laptop', price: 999.99 },
       originalKey: 'NDI=',
       originalValue: '0g8MTGFwdG9wUrgehes/j0A=',
+    };
+    expect(records[0]).toEqual(expected);
+  });
+
+  it('should deserialise protobuf message', async () => {
+    const consumer = kafkaConsumer<Key, Product>(handler, {
+      value: {
+        type: 'protobuf',
+        schemaStr: `syntax = "proto3";
+          message Product {
+            int32 id = 1;
+            string name = 2;
+            double price = 3;
+          }`,
+        outputObject: 'Product',
+      },
+      key: {
+        type: 'json',
+        outputObject: keyObj,
+      },
+    });
+
+    const records = await consumer(protobufEvent, {});
+    const expected = {
+      key: 42,
+      value: { id: 1001, name: 'Laptop', price: 999.99 },
+      originalKey: 'NDI=',
+      originalValue: 'COkHEgZMYXB0b3AZUrgehes/j0A=',
     };
     expect(records[0]).toEqual(expected);
   });

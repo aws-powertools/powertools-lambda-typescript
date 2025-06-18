99b85381971d21607d40f68d86676c86c3e60f26
Alexander Schueren <sha@amazon.com>
2025-06-18 11:43:22 +0200

    move examples to a different project

--- examples/kafka/app/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const productSchema = z.object({
-  id: z.number(),
-  name: z.string(),
-  price: z.number().positive({
-    message: "Price can't be negative",
-  }),
-});

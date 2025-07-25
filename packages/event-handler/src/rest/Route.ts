b07208a327be544707ba4798395d9f63a00aad58
svozza <svozza@gmail.com>
2025-07-25 17:29:56 +0100

    remove registeredAt field in Route class

--- packages/event-handler/src/rest/Route.ts
+++ packages/event-handler/src/rest/Route.ts
@@ -5,14 +5,12 @@ class Route {
   readonly method: string;
   readonly path: Path;
   readonly handler: RouteHandler;
-  readonly registeredAt: Date;
 
   constructor(method: string, path: Path, handler: RouteHandler) {
     this.id = `${method}:${path}`;
     this.method = method.toUpperCase();
     this.path = path;
     this.handler = handler;
-    this.registeredAt = new Date();
   }
 }
 

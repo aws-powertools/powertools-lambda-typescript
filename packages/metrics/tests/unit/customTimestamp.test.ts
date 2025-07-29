ec8b0cd6f5932520c1628c9b4f8c595643c1a8a4
Stefano Vozza <svozza@gmail.com>
2025-07-29 12:59:50 +0100

    Revert "fix(metrics): emit warning when default dimensions are overwritten (#…"
    This reverts commit a933a6af6135b79b855353bbaf265ab0a3be60da.

--- packages/metrics/tests/unit/customTimestamp.test.ts
+++ packages/metrics/tests/unit/customTimestamp.test.ts
@@ -48,9 +48,7 @@ describe('Setting custom timestamp', () => {
 
   it('logs a warning when the provided timestamp is too far in the past', () => {
     // Prepare
-    const metrics = new Metrics({
-      singleMetric: true,
-    });
+    const metrics = new Metrics({ singleMetric: true });
 
     // Act
     metrics.setTimestamp(Date.now() - EMF_MAX_TIMESTAMP_PAST_AGE - 1000);
@@ -65,9 +63,7 @@ describe('Setting custom timestamp', () => {
 
   it('logs a warning when the provided timestamp is too far in the future', () => {
     // Prepare
-    const metrics = new Metrics({
-      singleMetric: true,
-    });
+    const metrics = new Metrics({ singleMetric: true });
 
     // Act
     metrics.setTimestamp(Date.now() + EMF_MAX_TIMESTAMP_FUTURE_AGE + 1000);

bc78e509093a9023006f7115e5de73ad5bcb6eaf
svozza <svozza@amazon.com>
2025-08-11 15:12:10 +0100

    add all http error codes

--- packages/event-handler/src/types/rest.ts
+++ packages/event-handler/src/types/rest.ts
@@ -1,13 +1,31 @@
 import type { GenericLogger } from '@aws-lambda-powertools/commons/types';
 import type { BaseRouter } from '../rest/BaseRouter.js';
-import type { HttpVerbs } from '../rest/constants.js';
+import type { HttpErrorCodes, HttpVerbs } from '../rest/constants.js';
 import type { Route } from '../rest/Route.js';
 
 type ErrorResponse = {
-  statusCode: number;
+  statusCode: HttpStatusCode;
   error: string;
   message: string;
 };
+
+interface ErrorContext {
+  path: string;
+  method: string;
+  headers: Record<string, string>;
+  timestamp: string;
+  requestId?: string;
+}
+
+type ErrorHandler<T extends Error = Error> = (
+  error: T,
+  context?: ErrorContext
+) => ErrorResponse;
+
+interface ErrorConstructor<T extends Error = Error> {
+  new (...args: any[]): T;
+  prototype: T;
+}
 
 /**
  * Options for the {@link BaseRouter} class
@@ -34,6 +52,8 @@ type DynamicRoute = Route & CompiledRoute;
 type RouteHandler<T = any, R = any> = (...args: T[]) => R;
 
 type HttpMethod = keyof typeof HttpVerbs;
+
+type HttpStatusCode = (typeof HttpErrorCodes)[keyof typeof HttpErrorCodes];
 
 type Path = `/${string}`;
 
@@ -66,6 +86,8 @@ export type {
   CompiledRoute,
   DynamicRoute,
   ErrorResponse,
+  ErrorConstructor,
+  ErrorHandler,
   HttpMethod,
   Path,
   RouterOptions,

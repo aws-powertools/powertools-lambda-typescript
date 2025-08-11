94f34552c7a11d34c97e68040cec456e3b7ba0a3
svozza <svozza@amazon.com>
2025-08-11 12:50:23 +0100

    add error classes for http errors

--- packages/event-handler/src/rest/constants.ts
+++ packages/event-handler/src/rest/constants.ts
@@ -10,6 +10,18 @@ export const HttpVerbs = {
   OPTIONS: 'OPTIONS',
 } as const;
 
+export const HttpErrorCodes = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  METHOD_NOT_ALLOWED: 405,
+  REQUEST_TIMEOUT: 408,
+  REQUEST_ENTITY_TOO_LARGE: 413,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503,
+} as const;
+
 export const PARAM_PATTERN = /:([a-zA-Z_]\w*)(?=\/|$)/g;
 
 export const SAFE_CHARS = "-._~()'!*:@,;=+&$";

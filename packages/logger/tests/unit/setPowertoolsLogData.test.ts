bfe97f147cb940e7dec785a5177707351d7cff1e
Max Opperman <max.opperman@alliander.com>
2025-08-13 12:40:11 +0200

    chore(logger): adhere to tests standard

--- packages/logger/tests/unit/setPowertoolsLogData.test.ts
+++ packages/logger/tests/unit/setPowertoolsLogData.test.ts
@@ -1,38 +1,44 @@
 import { Logger } from '../../src/Logger.js';
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, afterEach } from 'vitest';
 
-describe('Logger PowertoolsLogData AWS_REGION', () => {
+describe('Logger PowertoolsLogData environment variables', () => {
+  afterEach(() => {
+    vi.unstubAllEnvs();
+  });
+
   it.each([
     [undefined, ''],
     ['us-west-2', 'us-west-2'],
-  ])('should set awsRegion to %s when AWS_REGION env var is %s', (envValue, expected) => {
+  ])('sets awsRegion to %s when AWS_REGION env var is %s', (envValue, expected) => {
+    // Prepare
     vi.stubEnv('AWS_REGION', envValue);
+    // Act
     const logger = new Logger();
+    // Assess
     expect(logger['powertoolsLogData'].awsRegion).toBe(expected);
-    vi.unstubAllEnvs();
   });
-});
 
-describe('Logger PowertoolsLogData ENVIRONMENT', () => {
   it.each([
     [undefined, ''],
     ['prd', 'prd'],
-  ])('should set environment to %s when ENVIRONMENT env var is %s', (envValue, expected) => {
+  ])('sets environment to %s when ENVIRONMENT env var is %s', (envValue, expected) => {
+    // Prepare
     vi.stubEnv('ENVIRONMENT', envValue);
+    // Act
     const logger = new Logger();
+    // Assess
     expect(logger['powertoolsLogData'].environment).toBe(expected);
-    vi.unstubAllEnvs();
   });
-});
 
-describe('Logger PowertoolsLogData POWERTOOLS_SERVICE_NAME', () => {
   it.each([
     [undefined, 'service_undefined'],
     ['my-service', 'my-service'],
-  ])('should set serviceName to %s when POWERTOOLS_SERVICE_NAME env var is %s', (envValue, expected) => {
+  ])('sets serviceName to %s when POWERTOOLS_SERVICE_NAME env var is %s', (envValue, expected) => {
+    // Prepare
     vi.stubEnv('POWERTOOLS_SERVICE_NAME', envValue);
+    // Act
     const logger = new Logger();
+    // Assess
     expect(logger['powertoolsLogData'].serviceName).toBe(expected);
-    vi.unstubAllEnvs();
   });
 });

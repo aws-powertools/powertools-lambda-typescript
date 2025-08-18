4d7b12b6fca7bf4fee6fb2f688b014f6533968fc
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-08-18 23:40:00 +0530

    Fix DynamoDBProvider.ts duplicate imports

--- packages/parameters/src/dynamodb/DynamoDBProvider.ts
+++ packages/parameters/src/dynamodb/DynamoDBProvider.ts
@@ -1,13 +1,11 @@
 import type { JSONValue } from '@aws-lambda-powertools/commons/types';
-import type {
-  GetItemCommandInput,
-  QueryCommandInput,
-} from '@aws-sdk/client-dynamodb';
 import {
   DynamoDBClient,
   type DynamoDBPaginationConfiguration,
   GetItemCommand,
+  type GetItemCommandInput,
   paginateQuery,
+  type QueryCommandInput,
 } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import { BaseProvider } from '../base/BaseProvider.js';

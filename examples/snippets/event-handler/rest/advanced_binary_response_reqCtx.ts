0630e1fe53b8f8fb9790dd06486838d8b9b29cd4
svozza <svozza@amazon.com>
2025-11-20 17:49:53 +0000

    doc(event-handler): update binary response docs

---
+++ examples/snippets/event-handler/rest/advanced_binary_response_reqCtx.ts
@@ -0,0 +1,12 @@
+import { Router } from '@aws-lambda-powertools/event-handler/experimental-rest';
+import type { Context } from 'aws-lambda';
+
+const app = new Router();
+
+app.get('/json64', async (reqCtx) => {
+  reqCtx.isBase64Encoded = true;
+  return { message: 'Hello World!' };
+});
+
+export const handler = async (event: unknown, context: Context) =>
+  app.resolve(event, context);

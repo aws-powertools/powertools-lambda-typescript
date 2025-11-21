ce213022743e56d982b9c4973232747fb3821b1f
Swopnil Dangol <swopnildangol@gmail.com>
2025-11-21 19:05:06 +0000

    Bumped the layer verison from 40 to 41

--- examples/app/cdk/example-stack.ts
+++ examples/app/cdk/example-stack.ts
@@ -39,7 +39,7 @@ export class PowertoolsExampleStack extends Stack {
       'powertools-layer',
       `arn:aws:lambda:${
         Stack.of(this).region
-      }:094274105915:layer:AWSLambdaPowertoolsTypeScriptV2:40`
+      }:094274105915:layer:AWSLambdaPowertoolsTypeScriptV2:41`
     );
 
     // Items table

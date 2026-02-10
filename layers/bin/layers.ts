1dc572ed5edbfca6da54b90c6cfb7c2d861f6def
Andrea Amorosi <dreamorosi@gmail.com>
2026-02-10 16:06:03 +0100

    ci: fix layer import in CDK stack (#5009)
    Co-authored-by: Stefano Vozza <svozza@amazon.com>

--- layers/bin/layers.ts
+++ layers/bin/layers.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
+import 'source-map-support/register.js';
 import { App } from 'aws-cdk-lib';
 import { CanaryStack } from '../src/canary-stack.js';
 import { LayerPublisherStack } from '../src/layer-publisher-stack.js';

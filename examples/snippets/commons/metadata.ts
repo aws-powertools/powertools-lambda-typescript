bdfa5830ec5a315b5f5e687b2cac876bc2e2a748
Stefano Vozza <svozza@amazon.com>
2026-03-18 20:42:00 +0000

    feat(commons): add Lambda Metadata Service support (#5106)

---
+++ examples/snippets/commons/metadata.ts
@@ -0,0 +1,10 @@
+import { getMetadata } from '@aws-lambda-powertools/commons/utils/metadata';
+import { Logger } from '@aws-lambda-powertools/logger';
+
+const logger = new Logger({ serviceName: 'serverlessAirline' });
+const metadata = await getMetadata();
+
+export const handler = async () => {
+  const { AvailabilityZoneId: azId } = metadata;
+  logger.appendKeys({ azId });
+};

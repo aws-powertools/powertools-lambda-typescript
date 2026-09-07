68ebc7c4a453c461ba818a770e7dd0a03aae610b
Andrea Amorosi <dreamorosi@gmail.com>
2026-09-07 14:09:54 +0200

    chore(maintenance): retire sample application (#5634)

--- examples/app/functions/commons/types.ts
+++
@@ -1,7 +0,0 @@
-import type { LogItemExtraInput } from '@aws-lambda-powertools/logger/types';
-
-type DebugLogger = {
-  debug: (message: string, ...extraInput: LogItemExtraInput) => void;
-};
-
-export type { DebugLogger };
